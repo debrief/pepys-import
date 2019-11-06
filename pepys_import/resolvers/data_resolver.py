fbef33d5b36450c03dccf98d3814d478cf94d38b
Bayram Barış Sarı <bayrambariss@gmail.com>
2019-11-06 15:42:17 +0300

    code refactored

---
+++ pepys_import/resolvers/data_resolver.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+
+class DataResolver(ABC):
+    @abstractmethod
+    def resolve_platform(self, data_store, platform_name):
+        # Implementation method should return any data necessary to create a
+        # platform
+        # Currently: platformName, platformType, nationality. Probably
+        # hostPlatform when that is needed
+        pass
+
+    @abstractmethod
+    def resolve_sensor(self, data_store, sensor_name):
+        # Implementation method should return any data necessary to create a
+        # sensor
+        # Currently: sensorName, sensorType
+        pass
+
+    @abstractmethod
+    def resolve_privacy(self, data_store, table_type_id, table_name):
+        # Implementation method should return any data necessary to create a
+        # privacy
+        # Currently: tabletypeId, privacyName
+        pass
