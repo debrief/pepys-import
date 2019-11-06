20fca0a8606c7c0cfa4c3d97065f0d5c1d5dd8f5
Bayram Barış Sarı <bayrambariss@gmail.com>
2019-11-06 18:13:54 +0300

    black applied

--- pepys_import/core/formats/state.py
+++ pepys_import/core/formats/state.py
@@ -4,7 +4,6 @@ from . import unit_registry, quantity
 
 
 class State:
-
     def __init__(self, line_number, line):
         self.line_num = line_number
         self.line = line
@@ -23,16 +22,31 @@ class State:
         self.unit_registry = unit_registry
 
     def print(self):
-        print("REP Line {} - Timestamp: {} Vessel: {} Symbology: {} Latitude: {} Longitude: {} Heading: {} Speed: {} Depth: {} TextLabel: {}"
-              .format(self.line_num, self.timestamp, self.vessel, self.symbology,
-                      self.latitude, self.longitude, self.heading, self.speed, self.depth, self.text_label))
+        print(
+            "REP Line {} - Timestamp: {} Vessel: {} Symbology: {} Latitude: {} Longitude: {} Heading: {} Speed: {} Depth: {} TextLabel: {}".format(
+                self.line_num,
+                self.timestamp,
+                self.vessel,
+                self.symbology,
+                self.latitude,
+                self.longitude,
+                self.heading,
+                self.speed,
+                self.depth,
+                self.text_label,
+            )
+        )
 
     def parse(self):
 
         tokens = self.line.split()
 
         if len(tokens) < 15:
-            print("Error on line {} not enough tokens: {}".format(self.line_num, self.line))
+            print(
+                "Error on line {} not enough tokens: {}".format(
+                    self.line_num, self.line
+                )
+            )
             return False
 
         # separate token strings
@@ -58,12 +72,20 @@ class State:
             self.text_label = tokens[15:]
 
         if len(date_token) != 6 and len(date_token) != 8:
-            print("Line {}. Error in Date format {}. Should be either 2 of 4 figure date, followed by month then date".format(self.line_num, date_token))
+            print(
+                "Line {}. Error in Date format {}. Should be either 2 of 4 figure date, followed by month then date".format(
+                    self.line_num, date_token
+                )
+            )
             return False
 
         # Times always in Zulu/GMT
-        if len(time_token) != 6 and  len(time_token) != 10:
-            print("Line {}. Error in Time format {}. Should be HHMMSS[.SSS]".format(self.line_num, time_token))
+        if len(time_token) != 6 and len(time_token) != 10:
+            print(
+                "Line {}. Error in Time format {}. Should be HHMMSS[.SSS]".format(
+                    self.line_num, time_token
+                )
+            )
             return False
 
         self.timestamp = self.parse_timestamp(date_token, time_token)
@@ -73,20 +95,32 @@ class State:
         symbology_values = symbology_token.split("[")
         if len(symbology_values) >= 1:
             if len(symbology_values[0]) != 2 and len(symbology_values[0]) != 5:
-                print("Line {}. Error in Symbology format {}. Should be 2 or 5 chars".format(self.line_num, symbology_token))
+                print(
+                    "Line {}. Error in Symbology format {}. Should be 2 or 5 chars".format(
+                        self.line_num, symbology_token
+                    )
+                )
                 return False
         if len(symbology_values) != 1 and len(symbology_values) != 2:
-            print("Line {}. Error in Symbology format {}".format(self.line_num, symbology_token))
+            print(
+                "Line {}. Error in Symbology format {}".format(
+                    self.line_num, symbology_token
+                )
+            )
             return False
 
         self.symbology = symbology_token
 
-        self.latitude = Location(lat_degrees_token, lat_mins_token, lat_secs_token, lat_hemi_token)
+        self.latitude = Location(
+            lat_degrees_token, lat_mins_token, lat_secs_token, lat_hemi_token
+        )
         if not self.latitude.parse():
             print("Line {}. Error in latitude parsing".format(self.line_num))
             return False
 
-        self.longitude = Location(long_degrees_token, long_mins_token, long_secs_token, long_hemi_token)
+        self.longitude = Location(
+            long_degrees_token, long_mins_token, long_secs_token, long_hemi_token
+        )
         if not self.latitude.parse():
             print("Line {}. Error in longitude parsing".format(self.line_num))
             return False
@@ -94,10 +128,18 @@ class State:
         try:
             valid_heading = float(heading_token)
         except ValueError:
-            print("Line {}. Error in heading value {}. Couldn't convert to a number".format(self.line_num, heading_token))
+            print(
+                "Line {}. Error in heading value {}. Couldn't convert to a number".format(
+                    self.line_num, heading_token
+                )
+            )
             return False
         if 0.0 > valid_heading >= 360.0:
-            print("Line {}. Error in heading value {}. Should be be between 0 and 359.9 degrees".format(self.line_num, heading_token))
+            print(
+                "Line {}. Error in heading value {}. Should be be between 0 and 359.9 degrees".format(
+                    self.line_num, heading_token
+                )
+            )
             return False
 
         # Set heading as degree(quantity-with-unit) object
@@ -106,45 +148,53 @@ class State:
         try:
             valid_speed = float(speed_token)
         except ValueError:
-            print("Line {}. Error in speed value {}. Couldn't convert to a number".format(self.line_num, speed_token))
+            print(
+                "Line {}. Error in speed value {}. Couldn't convert to a number".format(
+                    self.line_num, speed_token
+                )
+            )
             return False
-        
+
         # Set speed as knots(quantity-with-unit) object
         self.set_speed(valid_speed * self.unit_registry.knot)
 
         try:
-            if depth_token == 'NaN':
+            if depth_token == "NaN":
                 self.depth = 0.0
             else:
                 self.depth = float(depth_token)
         except ValueError:
-            print("Line {}. Error in depth value {}. Couldn't convert to a number".format(self.line_num, depth_token))
+            print(
+                "Line {}. Error in depth value {}. Couldn't convert to a number".format(
+                    self.line_num, depth_token
+                )
+            )
             return False
 
         return True
 
     def parse_timestamp(self, date, time):
         if len(date) == 6:
-            formatStr = '%y%m%d'
+            formatStr = "%y%m%d"
         else:
-            formatStr = '%Y%m%d'
+            formatStr = "%Y%m%d"
 
         if len(time) == 6:
-            formatStr += '%H%M%S'
+            formatStr += "%H%M%S"
         else:
-            formatStr += '%H%M%S.%f'
+            formatStr += "%H%M%S.%f"
 
         return datetime.strptime(date + time, formatStr)
 
     def set_speed(self, speed):
         self.speed = speed
 
-    def set_heading(self, heading : quantity):
+    def set_heading(self, heading: quantity):
         self.heading = heading
 
     def set_latitude(self):
         pass
-    
+
     def set_longitude(self):
         pass
 
