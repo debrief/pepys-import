4bfff179bb0fe00ac9cddd7772265ce459b4d070
Bayram Barış Sarı <bayrambariss@gmail.com>
2019-11-06 17:59:50 +0300

    method name changed

--- pepys_import/core/formats/__init__.py
+++ pepys_import/core/formats/__init__.py
@@ -2,5 +2,5 @@ from .unit_conversion import UnitConversion
 
 # Initializing Pint's Unit Registry
 conversion = UnitConversion()
-unit_registry = conversion.getUnitRegistry()
+unit_registry = conversion.get_unit_registry()
 quantity = unit_registry.Quantity
