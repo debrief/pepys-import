27aee955540bba785f73a6a8afae5b400f3d9e7b
Bayram Barış Sarı <bayrambariss@gmail.com>
2019-11-06 17:36:22 +0300

    typos and issues corrected

--- pepys_import/core/store/data_store.py
+++ pepys_import/core/store/data_store.py
@@ -34,17 +34,18 @@ class DataStore:
         missing_data_resolver=DefaultResolver(),
     ):
         if db_type == "postgres":
-            self.db_classes = import_module("Store.PostgresDB")
+            self.db_classes = import_module("pepys_import.core.store.postgres_db")
             driver = "postgresql+psycopg2"
         elif db_type == "sqlite":
-            self.db_classes = import_module("Store.SqliteDB")
+            self.db_classes = import_module("pepys_import.core.store.sqlite_db")
             driver = "sqlite+pysqlite"
         else:
             raise Exception(
                 f"Unknown db_type {db_type} supplied, if specified should be one of 'postgres' or 'sqlite'"
             )
 
-        # setup table type data
+        # setup meta_class data
+        self.meta_classes = {}
         self.setup_table_type_mapping()
 
         connection_string = "{}://{}:{}@{}:{}/{}".format(
@@ -69,7 +70,6 @@ class DataStore:
         self.platforms = {}
         self.sensor_types = {}
         self.sensors = {}
-        self.metaClasses = {}
 
         # TEMP list of values for defaulted IDs, to be replaced by missing info lookup mechanism
         self.default_user_id = 1  # DevUser
@@ -289,7 +289,7 @@ class DataStore:
             reference=datafile_name,
             url=None,
             privacy_id=privacy.privacy_id,
-            datafiletype_id=datafile_type_obj.datafiletype_id,
+            datafile_type_id=datafile_type_obj.datafile_type_id,
         )
 
         self.session.add(datafile_obj)
@@ -474,7 +474,7 @@ class DataStore:
             datafile_id=entry_id,
             simulated=bool(simulated),
             privacy_id=privacy.privacy_id,
-            datafiletype_id=datafile_type.datafiletype_id,
+            datafile_type_id=datafile_type.datafile_type_id,
             reference=reference,
             url=url,
         )
@@ -714,7 +714,7 @@ class DataStore:
             ]
         )
         for table_type in TableTypes:
-            self.metaClasses[table_type] = [
+            self.meta_classes[table_type] = [
                 cls
                 for name, cls in db_classes.items()
                 if db_classes[name].table_type == table_type
@@ -723,7 +723,7 @@ class DataStore:
     def get_table_type_data(self, table_types):
         data = dict()
         for table_type in table_types:
-            for table in self.metaClasses[table_type]:
+            for table in self.meta_classes[table_type]:
                 data[table.__name__] = self.session.query(table).count()
         return data
 
@@ -741,17 +741,17 @@ class DataStore:
         # Create reference table list
         with self.session_scope() as session:
             self.setup_table_type_mapping()
-            reference_table_objects = self.metaClasses[TableTypes.REFERENCE]
-            for object in list(reference_table_objects):
-                reference_tables.append(object.__tablename__)
+            reference_table_objects = self.meta_classes[TableTypes.REFERENCE]
+            for table_object in list(reference_table_objects):
+                reference_tables.append(table_object.__tablename__)
 
         reference_files = [
-            file for file in files if os.path.splitext(file)[0] in reference_tables
+            file for file in files if os.path.splitext(file)[0].replace(" ", "") in reference_tables
         ]
         for file in reference_files:
             # split file into filename and extension
             table_name, _ = os.path.splitext(file)
-            possible_method = "addTo" + table_name
+            possible_method = "add_to_" + table_name.lower().replace(" ", "_")
             method_to_call = getattr(self, possible_method, None)
             if method_to_call:
                 with open(os.path.join(reference_data_folder, file), "r") as f:
@@ -765,7 +765,7 @@ class DataStore:
                 print(f"Method({possible_method}) not found!")
 
     def populate_metadata(self, sample_data_folder=None):
-        """Import CSV files from the given folder to the realted Metadata Tables"""
+        """Import CSV files from the given folder to the related Metadata Tables"""
         if sample_data_folder is None:
             sample_data_folder = os.path.join("..", "default_data")
 
@@ -775,9 +775,9 @@ class DataStore:
         # Create metadata table list
         with self.session_scope() as session:
             self.setup_table_type_mapping()
-            metadata_table_objects = self.metaClasses[TableTypes.METADATA]
-            for object in list(metadata_table_objects):
-                metadata_tables.append(object.__tablename__)
+            metadata_table_objects = self.meta_classes[TableTypes.METADATA]
+            for table_object in list(metadata_table_objects):
+                metadata_tables.append(table_object.__tablename__)
 
         metadata_files = [
             file for file in files if os.path.splitext(file)[0] in metadata_tables
@@ -785,7 +785,7 @@ class DataStore:
         for file in sorted(metadata_files):
             # split file into filename and extension
             table_name, _ = os.path.splitext(file)
-            possible_method = "addTo" + table_name
+            possible_method = "add_to_" + table_name.lower().replace(" ", "_")
             method_to_call = getattr(self, possible_method, None)
             if method_to_call:
                 with open(os.path.join(sample_data_folder, file), "r") as f:
