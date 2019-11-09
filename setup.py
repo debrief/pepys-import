f50a134c6e79f114a8d8f460c74536bf9f7ad026
Ian Mayo <ian@planetmayo.com>
2019-11-09 12:57:42 +0000

    Ian thinks that bump2version mangled the version numbers here, preventing pyPi from uploading.

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/pepys_import",
-    version="version='0.0.2'",
+    version="0.0.2",
     zip_safe=False,
 )
