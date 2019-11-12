8922bc68315dedd2dac56963e200ff73decd1e21
Bayram Barış Sarı <bayrambariss@gmail.com>
2019-11-12 21:12:57 +0300

    unittest.skip deleted

--- tests/test_data_store_initialise.py
+++ tests/test_data_store_initialise.py
@@ -22,7 +22,6 @@ class TestDataStoreInitialisePostgres(TestCase):
         except AttributeError:
             return
 
-    @unittest.skip("Skip until PostGIS is setup")
     def test_postgres_initialise(self):
         """Test whether schemas created successfully on PostgresSQL"""
         if self.store is None:
