15de49b6c54c568e99d0787e5ecba54da815d225
Bayram Barış Sarı <bayrambariss@gmail.com>
2019-11-14 12:51:29 +0300

    system specific paths added

--- pepys_import/utils/geoalchemy_utils.py
+++ pepys_import/utils/geoalchemy_utils.py
@@ -1,4 +1,13 @@
-EXTENSION_PATH = "/usr/lib/x86_64-linux-gnu/mod_spatialite.so"
+import platform
+
+SYSTEM = platform.system()
+if SYSTEM == "Linux":
+    EXTENSION_PATH = "/usr/lib/x86_64-linux-gnu/mod_spatialite.so"
+elif SYSTEM == "Darwin":  # Darwin is MacOS
+    EXTENSION_PATH = "/usr/local/lib/mod_spatialite.dylib"
+elif SYSTEM == "Windows":
+    # TODO: mod_spatialite path for Windows should be added
+    EXTENSION_PATH = None
 
 
 def load_spatialite(connection, connection_record):
