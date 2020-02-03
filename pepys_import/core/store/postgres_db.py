c58ed18ec6d0a261713075b63fb70bd3544c0328
BarisSari <sariba@itu.edu.tr>
2020-02-03 14:13:12 +0300

    Create placeholder classes for not implemented tables

--- pepys_import/core/store/postgres_db.py
+++ pepys_import/core/store/postgres_db.py
@@ -31,18 +31,12 @@ class TableType(base):
     name = Column(String(150))
 
 
-class SensorType(base):
-    __tablename__ = "SensorTypes"
-    table_type = TableTypes.REFERENCE
-
-    sensor_type_id = Column(
-        UUID(as_uuid=True), primary_key=True, server_default=FetchedValue()
-    )
-    # TODO: does this, or other string limits need checking or validating on file import?
-    name = Column(String(150))
+# Metadata Tables
+class HostedBy(base):
+    pass
 
 
-class Sensor(base):
+class Sensors(base):
     __tablename__ = "Sensors"
     table_type = TableTypes.METADATA
     table_type_id = 2  # Only needed for tables referenced by Entry table
@@ -55,19 +49,7 @@ class Sensor(base):
     platform_id = Column(UUID, nullable=False)
 
 
-class PlatformType(base):
-    __tablename__ = "PlatformTypes"
-    table_type = TableTypes.REFERENCE
-
-    platform_type_id = Column(
-        UUID(as_uuid=True), primary_key=True, server_default=FetchedValue()
-    )
-    # TODO: does this, or other string limits need checking or validating on file import?
-    name = Column(String(150))
-    # TODO: add relationships and ForeignKey entries to auto-create Entry ids
-
-
-class Platform(base):
+class Platforms(base):
     __tablename__ = "Platforms"
     table_type = TableTypes.METADATA
     table_type_id = 1  # Only needed for tables referenced by Entry table
@@ -85,18 +67,15 @@ class Platform(base):
     privacy_id = Column(UUID(as_uuid=True), nullable=False)
 
 
-class DatafileType(base):
-    __tablename__ = "DatafileTypes"
-    table_type = TableTypes.REFERENCE
-
-    datafile_type_id = Column(
-        UUID(as_uuid=True), primary_key=True, server_default=FetchedValue()
-    )
-    # TODO: does this, or other string limits need checking or validating on file import?
-    name = Column(String(150), nullable=False)
+class Tasks(base):
+    pass
 
 
-class Datafile(base):
+class Participants(base):
+    pass
+
+
+class Datafiles(base):
     __tablename__ = "Datafiles"
     table_type = TableTypes.METADATA
     table_type_id = 4  # Only needed for tables referenced by Entry table
@@ -113,7 +92,127 @@ class Datafile(base):
     # TODO: add relationships and ForeignKey entries to auto-create Entry ids
 
 
-class State(base):
+class Synonyms(base):
+    pass
+
+
+class Changes(base):
+    pass
+
+
+class Log(base):
+    pass
+
+
+class Extractions(base):
+    pass
+
+
+class Tags(base):
+    pass
+
+
+class TaggedItems(base):
+    pass
+
+
+# Reference Tables
+class PlatformType(base):
+    __tablename__ = "PlatformTypes"
+    table_type = TableTypes.REFERENCE
+
+    platform_type_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default=FetchedValue()
+    )
+    # TODO: does this, or other string limits need checking or validating on file import?
+    name = Column(String(150))
+    # TODO: add relationships and ForeignKey entries to auto-create Entry ids
+
+
+class Nationalities(base):
+    __tablename__ = "Nationalities"
+    table_type = TableTypes.REFERENCE
+
+    nationality_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default=FetchedValue()
+    )
+    name = Column(String(150), nullable=False)
+
+
+class GeometryTypes(base):
+    pass
+
+
+class GeometrySubTypes(base):
+    pass
+
+
+class Users(base):
+    pass
+
+
+class UnitTypes(base):
+    pass
+
+
+class ClassificationTypes(base):
+    pass
+
+
+class ContactTypes(base):
+    pass
+
+
+class SensorTypes(base):
+    __tablename__ = "SensorTypes"
+    table_type = TableTypes.REFERENCE
+
+    sensor_type_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default=FetchedValue()
+    )
+    # TODO: does this, or other string limits need checking or validating on file import?
+    name = Column(String(150))
+
+
+class Privacies(base):
+    __tablename__ = "Privacies"
+    table_type = TableTypes.REFERENCE
+
+    privacy_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default=FetchedValue()
+    )
+    name = Column(String(150), nullable=False)
+
+
+class DatafileTypes(base):
+    __tablename__ = "DatafileTypes"
+    table_type = TableTypes.REFERENCE
+
+    datafile_type_id = Column(
+        UUID(as_uuid=True), primary_key=True, server_default=FetchedValue()
+    )
+    # TODO: does this, or other string limits need checking or validating on file import?
+    name = Column(String(150), nullable=False)
+
+
+class MediaTypes(base):
+    pass
+
+
+class CommentTypes(base):
+    pass
+
+
+class CommodityTypes(base):
+    pass
+
+
+class ConfidenceLevels(base):
+    pass
+
+
+# Measurements Tables
+class States(base):
     __tablename__ = "State"
     table_type = TableTypes.MEASUREMENT
     table_type_id = 3  # Only needed for tables referenced by Entry table
@@ -130,23 +229,3 @@ class State(base):
     speed = Column(DOUBLE_PRECISION)
     datafile_id = Column(UUID(as_uuid=True), nullable=False)
     privacy_id = Column(UUID(as_uuid=True))
-
-
-class Nationality(base):
-    __tablename__ = "Nationalities"
-    table_type = TableTypes.REFERENCE
-
-    nationality_id = Column(
-        UUID(as_uuid=True), primary_key=True, server_default=FetchedValue()
-    )
-    name = Column(String(150), nullable=False)
-
-
-class Privacy(base):
-    __tablename__ = "Privacies"
-    table_type = TableTypes.REFERENCE
-
-    privacy_id = Column(
-        UUID(as_uuid=True), primary_key=True, server_default=FetchedValue()
-    )
-    name = Column(String(150), nullable=False)
