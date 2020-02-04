343c2b466fff902935b37d70d23accb5843bb1af
BarisSari <sariba@itu.edu.tr>
2020-02-04 14:43:13 +0300

    Develop platform related tests

--- tests/test_data_store_new_api.py
+++ tests/test_data_store_new_api.py
@@ -8,6 +8,12 @@ class TestDataStore(TestCase):
     def setUp(self):
         self.store = DataStore("", "", "", 0, ":memory:", db_type="sqlite")
         self.store.initialise()
+        with self.store.session_scope() as session:
+            self.nationality = self.store.add_to_nationalities("test_nationality").name
+            self.platform_type = self.store.add_to_platform_types(
+                "test_platform_type"
+            ).name
+            self.privacy = self.store.add_to_privacies("test_privacy").name
 
     def tearDown(self):
         pass
@@ -48,13 +54,61 @@ class TestDataStore(TestCase):
         self.assertEqual(len(datafiles), 1)
 
     @unittest.skip("Skip until missing data resolver is implemented.")
-    def test_resolver_works_for_datafile(self):
+    def test_missing_data_resolver_works_for_datafile(self):
         pass
 
     def test_new_platform_added_successfully(self):
-        pass
+        """Test whether a new platform is created successfully or not"""
+
+        with self.store.session_scope() as session:
+            platforms = self.store.get_platforms()
+
+        # there must be no entry at the beginning
+        self.assertEqual(len(platforms), 0)
+
+        with self.store.session_scope() as session:
+            created_platform = self.store.get_platform(
+                "Test Platform",
+                nationality=self.nationality,
+                platform_type=self.platform_type,
+                privacy=self.privacy,
+            )
+
+        # there must be one entry
+        with self.store.session_scope() as session:
+            platforms = self.store.get_platforms()
+        self.assertEqual(len(platforms), 1)
 
     def test_present_platform_not_added(self):
+        """Test whether present platform is not created"""
+
+        with self.store.session_scope() as session:
+            platforms = self.store.get_platforms()
+
+        # there must be no entry at the beginning
+        self.assertEqual(len(platforms), 0)
+
+        with self.store.session_scope() as session:
+            created_platform = self.store.get_platform(
+                "Test Platform",
+                nationality=self.nationality,
+                platform_type=self.platform_type,
+                privacy=self.privacy,
+            )
+            created_platform_2 = self.store.get_platform(
+                "Test Platform",
+                nationality=self.nationality,
+                platform_type=self.platform_type,
+                privacy=self.privacy,
+            )
+
+        # there must be one entry
+        with self.store.session_scope() as session:
+            platforms = self.store.get_platforms()
+        self.assertEqual(len(platforms), 1)
+
+    @unittest.skip("Skip until missing data resolver is implemented.")
+    def test_missing_data_resolver_works_for_platform(self):
         pass
 
     def test_new_sensor_added_successfully(self):
