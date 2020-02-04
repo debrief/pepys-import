54d4ccc02eb348adffb178a2f39155b57ce0076f
BarisSari <sariba@itu.edu.tr>
2020-02-04 22:24:37 +0300

    Add todos

--- pepys_import/core/store/table_summary.py
+++ pepys_import/core/store/table_summary.py
@@ -1,8 +1,10 @@
+# TODO: it doesn't return creation date of last item added.
 def table_summary(session, table_name):
     number_of_rows = session.query(table_name).count()
     return number_of_rows
 
 
+# TODO: not implemented yet
 class TableSummarySet(object):
     def report(self):
         pass
