2c384a1a60eb4a4e546ba67b477bab7f357e6c26
BarisSari <sariba@itu.edu.tr>
2020-02-08 13:55:18 +0300

    Add docstrings, implement placeholder table_delta function

--- pepys_import/core/store/table_summary.py
+++ pepys_import/core/store/table_summary.py
@@ -1,4 +1,14 @@
+from tabulate import tabulate
+
+
 def table_summary(session, table_name):
+    """
+    A summary of the contents of a table.
+
+    :param session: Bounded session for querying table
+    :param table_name: Name of the table
+    :return: Number of rows and creation date of last item added
+    """
     number_of_rows = session.query(table_name).count()
     last_row = (
         session.query(table_name).order_by(table_name.created_date.desc()).first()
@@ -10,9 +20,36 @@ def table_summary(session, table_name):
 
 
 # TODO: not implemented yet
+def table_delta(first_table, second_table):
+    """
+    A listing of changes between two TableSummarySet objects.
+
+    :param first_table: First TableSummarySet object
+    :param second_table: Second TableSummarySet object
+    :return: Change in number of rows
+    """
+    pass
+
+
+# TODO: not implemented yet
 class TableSummarySet(object):
+    """A collection of TableSummary elements."""
+
+    def __init__(self, table_summaries):
+        self.table_summaries = table_summaries
+        self.headers = ["Table name", "Number of rows", "Last item added"]
+
     def report(self):
-        pass
+        """Produce an HTML pretty-printed report of the contents of the summary."""
+
+        print(
+            tabulate(
+                [(k,) + v for k, v in self.table_summaries.items()],
+                headers=self.headers,
+                tablefmt="pretty",
+            )
+        )
 
     def compare_to(self, other: "TableSummarySet"):
+        """Produce an HTML pretty-printed report of the contents of the summary."""
         pass
