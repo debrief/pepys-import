4aef05412044f29e5a2fc6800c017d6a2eae9824
BarisSari <sariba@itu.edu.tr>
2020-02-13 22:37:04 +0300

    Correct test_line_ok and test_error_reports tests

--- tests/test_rep_file_parse.py
+++ tests/test_rep_file_parse.py
@@ -2,6 +2,7 @@ import unittest
 import datetime
 
 from pepys_import.core.formats import unit_registry
+from pepys_import.core.formats.location import Location
 from pepys_import.core.formats.state import State
 
 
@@ -26,84 +27,79 @@ class BasicTests(unittest.TestCase):
     def test_error_reports(self):
         # too few fields
         rep_line = State(1, " 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 ")
-        rep_line.parse()
+        self.assertFalse(rep_line.parse())
 
         # wrong length date
         rep_line = State(
             1, "12 120800 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 "
         )
-        rep_line.parse()
+        self.assertFalse(rep_line.parse())
 
         # wrong length time
         rep_line = State(
             1, "951212 12 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 "
         )
-        rep_line.parse()
+        self.assertFalse(rep_line.parse())
 
         # wrong length symbology
         rep_line = State(
             1,
             "951212 120800 SUBJECT VCC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 ",
         )
-        rep_line.parse()
+        self.assertFalse(rep_line.parse())
 
         # bad latitude
         rep_line = State(
             1,
             "951212 120800 SUBJECT VC 6A 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 ",
         )
-        rep_line.parse()
+        self.assertFalse(rep_line.parse())
 
-        # bad longtude
+        # bad longitude
         rep_line = State(
             1,
             "951212 120800 SUBJECT VC 60 23 40.25 N 00A 01 2B.86 E 109.08  6.00  0.00 ",
         )
-        rep_line.parse()
+        self.assertFalse(rep_line.parse())
 
         # bad course
         rep_line = State(
             1,
             "951212 120800 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 10a.08  6.00  0.00 ",
         )
-        rep_line.parse()
+        self.assertFalse(rep_line.parse())
 
         # bad speed
         rep_line = State(
             1,
             "951212 120800 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.b0  0.00 ",
         )
-        rep_line.parse()
+        self.assertFalse(rep_line.parse())
 
         # bad depth
         rep_line = State(
             1,
             "951212 120800 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.c0 ",
         )
-        rep_line.parse()
+        self.assertFalse(rep_line.parse())
 
     def test_line_ok(self):
         rep_line = State(
             1,
             "100112 120800 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 ",
         )
-        rep_line.parse()
+        self.assertTrue(rep_line.parse())
 
-        self.assertEqual(1, rep_line.get_line_number())
-
-        uReg = unit_registry
-
-        self.assertEqual(
-            datetime.datetime(2010, 1, 12, 12, 8), rep_line.get_timestamp()
-        )
+        self.assertEqual(1, rep_line.line_num)
+        self.assertEqual(datetime.datetime(2010, 1, 12, 12, 8), rep_line.timestamp)
         self.assertEqual("SUBJECT", rep_line.get_platform())
-        self.assertEqual("VC", rep_line.get_symbology())
-        # FixMe   self.assertEqual(Location(60.0, 23.0, 40.25, "N"), rep_line.getLatitude())
-        # FixMe   self.assertEqual("SUBJECT", rep_line.getLongitude())
-        self.assertAlmostEqual(1.9038051480754146, rep_line.get_heading())
-        self.assertAlmostEqual(6.00, rep_line.get_speed().to(uReg.knot).magnitude)
-        self.assertEqual(0.0, rep_line.get_depth())
-        self.assertEqual(None, rep_line.get_text_label())
+        self.assertEqual("VC", rep_line.symbology)
+        self.assertEqual(Location(60.0, 23.0, 40.25, "N"), rep_line.latitude)
+        self.assertEqual(Location("000", "01", "25.86", "E"), rep_line.longitude)
+        self.assertAlmostEqual(1.9038051480754146, rep_line.heading)
+        self.assertAlmostEqual(6.00, rep_line.speed.to(unit_registry.knot).magnitude)
+        self.assertEqual(0.0, rep_line.depth)
+        self.assertEqual(None, rep_line.text_label)
 
 
 if __name__ == "__main__":
