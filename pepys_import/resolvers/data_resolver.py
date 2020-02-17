7b24df94fa5191453623e85e19572c0671a54c4d
BarisSari <sariba@itu.edu.tr>
2020-02-17 12:25:57 +0300

    Remove pass, update docstrings

--- pepys_import/resolvers/data_resolver.py
+++ pepys_import/resolvers/data_resolver.py
@@ -6,22 +6,22 @@ class DataResolver(ABC):
     def resolve_platform(
         self, data_store, platform_name, platform_type_str, nationality_str, privacy_str
     ):
-        # Implementation method should return any data necessary to create a
-        # platform
-        # Currently: platformName, platformType, nationality. Probably
-        # hostPlatform when that is needed
-        pass
+        """
+        Implementation method should return any data necessary to create a platform.
+        Currently: platformName, platformType, nationality.
+        Probably host when that is needed
+        """
 
     @abstractmethod
     def resolve_sensor(self, data_store, sensor_name):
-        # Implementation method should return any data necessary to create a
-        # sensor
-        # Currently: sensorName, sensorType
-        pass
+        """
+        Implementation method should return any data necessary to create a sensor.
+        Currently: sensorName, sensorType
+        """
 
     @abstractmethod
     def resolve_privacy(self, data_store, table_type_id, table_name):
-        # Implementation method should return any data necessary to create a
-        # privacy
-        # Currently: tabletypeId, privacyName
-        pass
+        """
+        Implementation method should return any data necessary to create a privacy.
+        Currently: table_type_id, privacy_name
+        """
