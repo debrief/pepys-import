50243fd3c097e70a388b44ab3b43fd5dd2e97a27
BarisSari <sariba@itu.edu.tr>
2020-02-17 17:59:05 +0300

    Rename variables according to PEP8

--- pepys_import/resolvers/default_resolver.py
+++ pepys_import/resolvers/default_resolver.py
@@ -6,40 +6,41 @@ class DefaultResolver(DataResolver):
     default_platform_type = "Warship"
     default_nationality = "UK"
     default_sensor_type = "Position"
-    default_privacy = "Private"
+    default_privacy = "PRIVACY-1"
+    default_datafile_type = "DATAFILE-TYPE-1"
 
     def resolve_platform(
-        self, data_store, platformName, platform_type_str, nationality_str, privacy_str
+        self, data_store, platform_name, platform_type, nationality, privacy
     ):
         # needs to establish defaults for platform_type, nationality
-        if not platform_type_str:
-            platform_type_str = self.default_platform_type
-        platform_type = data_store.search_platform_type(platform_type_str)
         if not platform_type:
-            platform_type = data_store.add_to_platform_types(platform_type_str)
+            platform_type = self.default_platform_type
+        platform_type = data_store.search_platform_type(platform_type)
+        if not platform_type:
+            platform_type = data_store.add_to_platform_types(platform_type)
 
-        if not nationality_str:
-            nationality_str = self.default_nationality
-        nationality = data_store.search_nationality(nationality_str)
         if not nationality:
-            nationality = data_store.add_to_nationalities(nationality_str)
+            nationality = self.default_nationality
+        nationality = data_store.search_nationality(nationality)
+        if not nationality:
+            nationality = data_store.add_to_nationalities(nationality)
 
-        if not privacy_str:
-            privacy_str = self.default_privacy
-        privacy = data_store.search_privacy(privacy_str)
         if not privacy:
-            privacy = data_store.add_to_privacies(privacy_str)
+            privacy = self.default_privacy
+        privacy = data_store.search_privacy(privacy)
+        if not privacy:
+            privacy = data_store.add_to_privacies(privacy)
 
-        return platformName, platform_type, nationality, privacy
+        return platform_name, platform_type, nationality, privacy
 
-    def resolve_sensor(self, data_store, sensorName):
+    def resolve_sensor(self, data_store, sensor_name):
         # needs to establish defaults for sensorType
 
         sensor_type = data_store.search_sensor_type(self.default_sensor_type)
         if not sensor_type:
             sensor_type = data_store.add_to_sensor_types(self.default_sensor_type)
 
-        return sensorName, sensor_type
+        return sensor_name, sensor_type
 
     def resolve_privacy(self, data_store, table_type_id, table_name):
         # needs to establish defaults for privacy
