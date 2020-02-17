5670fdb6f546a8884a47d7b1c25e25cbcca37b3a
Ian Mayo <ian@planetmayo.com>
2020-02-17 19:45:27 +0000

    Test importing single etrac file works

--- tests/test_parse_etrac.py
+++ tests/test_parse_etrac.py
@@ -17,20 +17,45 @@ class ETracTests(unittest.TestCase):
         processor = FileProcessor()
 
         store = DataStore(
-            "", "", "", 0, "e_test.db", db_type="sqlite", show_welcome=False
+            "postgres", "postgres", "localhost", 5432, "test",
+            db_type="postgres", show_welcome=False
         )
         store.initialise()
         store.populate_reference(TEST_DATA_PATH)
-        store.populate_metadata(TEST_DATA_PATH)
-        store.populate_measurement(TEST_DATA_PATH)
 
         processor.register_importer(ETracParser())
 
-        # now good one
+        # check states empty
+        with store.session_scope():
+            # there must be no states at the beginning
+            states = store.session.query(store.db_classes.State).all()
+            self.assertEqual(len(states), 0)
+
+            # there must be no platforms at the beginning
+            platforms = store.session.query(store.db_classes.Platform).all()
+            self.assertEqual(len(platforms), 0)
+
+            # there must be no datafiles at the beginning
+            datafiles = store.session.query(store.db_classes.Datafile).all()
+            self.assertEqual(len(datafiles), 0)
+
+        # parse the folder
         processor.process(DATA_PATH, store, False)
+        # processor.process("Z:/MacDownloads/reetracdatafiles/db", store, False)
 
         # check data got created
+        with store.session_scope():
+            # there must be states after the import
+            states = store.session.query(store.db_classes.State).all()
+            self.assertEqual(len(states), 44)
 
+            # there must be platforms after the import
+            platforms = store.session.query(store.db_classes.Platform).all()
+            self.assertEqual(len(platforms), 18)
+
+            # there must be one datafile afterwards
+            datafiles = store.session.query(store.db_classes.Datafile).all()
+            self.assertEqual(len(datafiles), 1)
 
 if __name__ == "__main__":
     unittest.main()
