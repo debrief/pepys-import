089077440ba3e80d1942261eef6470a65b11ffb7
Ian Mayo <ian@planetmayo.com>
2020-02-18 16:07:21 +0000

    Switch to PostGres, since it handles higher volumes of entries

--- tests/test_parse_etrac.py
+++ tests/test_parse_etrac.py
@@ -1,11 +1,12 @@
 import os
 import unittest
+from sqlite3 import OperationalError
 
 from pepys_import.file.e_trac_parser import ETracParser
 from pepys_import.file.file_processor import FileProcessor
 from pepys_import.core.store.data_store import DataStore
 from testing.postgresql import Postgresql
-from sqlalchemy import inspect, event
+from sqlalchemy import event
 from pepys_import.core.store.db_base import BasePostGIS
 from sqlalchemy.sql.ddl import DropSchema
 
@@ -15,13 +16,11 @@ TEST_DATA_PATH = os.path.join(FILE_PATH, "sample_data", "csv_files")
 
 
 class ETracTests(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
+        self.postgres = None
         self.store = None
         try:
-            event.listen(
-                BasePostGIS.metadata, "before_create", DropSchema("datastore_schema"),
-            )
-            self.store = Postgresql(
+            self.postgres = Postgresql(
                 database="test",
                 host="localhost",
                 user="postgres",
@@ -30,13 +29,25 @@ class ETracTests(unittest.TestCase):
             )
         except RuntimeError:
             print("PostgreSQL database couldn't be created! Test is skipping.")
+            return
+        try:
+            self.store = DataStore(
+                db_name="test",
+                db_host="localhost",
+                db_username="postgres",
+                db_password="postgres",
+                db_port=55527,
+            )
+            self.store.initialise()
+        except OperationalError:
+            print("Database schema and data population failed! Test is skipping.")
 
-    def tearDown(self):
+    def tearDown(self) -> None:
         try:
             event.listen(
-                BasePostGIS.metadata, "before_create", DropSchema("datastore_schema"),
+                BasePostGIS.metadata, "before_create", DropSchema("datastore_schema")
             )
-            self.store.stop()
+            self.postgres.stop()
         except AttributeError:
             return
 
@@ -44,48 +55,41 @@ class ETracTests(unittest.TestCase):
         print("started")
         processor = FileProcessor()
 
-        store = DataStore(
-            db_username="postgres",
-            db_password="postgres",
-            db_host="localhost",
-            db_port=55527,
-            db_name="test",
-        )
 
-        store.initialise()
-        store.populate_reference(TEST_DATA_PATH)
+        # self.store.initialise()
+        # self.store.populate_reference(TEST_DATA_PATH)
 
         processor.register_importer(ETracParser())
 
         # check states empty
-        with store.session_scope():
+        with self.store.session_scope():
             # there must be no states at the beginning
-            states = store.session.query(store.db_classes.State).all()
+            states = self.store.session.query(self.store.db_classes.State).all()
             self.assertEqual(len(states), 0)
 
             # there must be no platforms at the beginning
-            platforms = store.session.query(store.db_classes.Platform).all()
+            platforms = self.store.session.query(self.store.db_classes.Platform).all()
             self.assertEqual(len(platforms), 0)
 
             # there must be no datafiles at the beginning
-            datafiles = store.session.query(store.db_classes.Datafile).all()
+            datafiles = self.store.session.query(self.store.db_classes.Datafile).all()
             self.assertEqual(len(datafiles), 0)
 
         # parse the folder
-        processor.process(DATA_PATH, store, False)
+        processor.process(DATA_PATH, self.store, False)
 
         # check data got created
-        with store.session_scope():
+        with self.store.session_scope():
             # there must be states after the import
-            states = store.session.query(store.db_classes.State).all()
+            states = self.store.session.query(self.store.db_classes.State).all()
             self.assertEqual(len(states), 44)
 
             # there must be platforms after the import
-            platforms = store.session.query(store.db_classes.Platform).all()
+            platforms = self.store.session.query(self.store.db_classes.Platform).all()
             self.assertEqual(len(platforms), 18)
 
             # there must be one datafile afterwards
-            datafiles = store.session.query(store.db_classes.Datafile).all()
+            datafiles = self.store.session.query(self.store.db_classes.Datafile).all()
             self.assertEqual(len(datafiles), 1)
 
 
