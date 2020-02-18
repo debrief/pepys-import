8adf7f9819f62b1fc1bab310a57017606bd4794b
Ian Mayo <ian@planetmayo.com>
2020-02-18 12:29:12 +0000

    Experiment with initialising database, and removing any existing schema, if necessary

--- tests/test_parse_etrac.py
+++ tests/test_parse_etrac.py
@@ -4,7 +4,10 @@ import unittest
 from pepys_import.file.e_trac_parser import ETracParser
 from pepys_import.file.file_processor import FileProcessor
 from pepys_import.core.store.data_store import DataStore
-
+from testing.postgresql import Postgresql
+from sqlalchemy import inspect, event
+from pepys_import.core.store.db_base import BasePostGIS
+from sqlalchemy.sql.ddl import DropSchema
 
 FILE_PATH = os.path.dirname(__file__)
 DATA_PATH = os.path.join(FILE_PATH, "sample_data/track_files/other_data")
@@ -12,14 +15,43 @@ TEST_DATA_PATH = os.path.join(FILE_PATH, "sample_data", "csv_files")
 
 
 class ETracTests(unittest.TestCase):
+    def setUp(self):
+        self.store = None
+        try:
+            event.listen(
+                BasePostGIS.metadata, "before_create", DropSchema("datastore_schema"),
+            )
+            self.store = Postgresql(
+                database="test",
+                host="localhost",
+                user="postgres",
+                password="postgres",
+                port=55527,
+            )
+        except RuntimeError:
+            print("PostgreSQL database couldn't be created! Test is skipping.")
+
+    def tearDown(self):
+        try:
+            event.listen(
+                BasePostGIS.metadata, "before_create", DropSchema("datastore_schema"),
+            )
+            self.store.stop()
+        except AttributeError:
+            return
+
     def test_process_e_trac_data(self):
         print("started")
         processor = FileProcessor()
 
         store = DataStore(
-            "postgres", "postgres", "localhost", 5432, "test",
-            db_type="postgres", show_welcome=False
+            db_username="postgres",
+            db_password="postgres",
+            db_host="localhost",
+            db_port=55527,
+            db_name="test",
         )
+
         store.initialise()
         store.populate_reference(TEST_DATA_PATH)
 
@@ -55,6 +87,7 @@ class ETracTests(unittest.TestCase):
             # there must be one datafile afterwards
             datafiles = store.session.query(store.db_classes.Datafile).all()
             self.assertEqual(len(datafiles), 1)
+
 
 if __name__ == "__main__":
     unittest.main()
