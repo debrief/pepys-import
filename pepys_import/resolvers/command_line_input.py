07f2b069ad46c127c4608492f423e3c27ef02565
BarisSari <sariba@itu.edu.tr>
2020-02-19 11:43:39 +0300

    Remove old function

--- pepys_import/resolvers/command_line_input.py
+++ pepys_import/resolvers/command_line_input.py
@@ -12,26 +12,3 @@ def create_menu(heading, choices):
     menu.add(".", "Cancel")
     choice = menu.show()
     return choice
-
-
-def get_choice_input(heading, choices):
-    map_choice = False
-    indexes = []
-    while 1:
-        input_text = heading + "\n"
-        for idx, choice in enumerate(choices, 1):
-            if isinstance(choice, list):
-                choice_string = choice[0]
-                map_choice = True
-            else:
-                choice_string = choice
-            input_text += f"   {str(idx)}) {choice_string}\n"
-            indexes.append(idx)
-
-        # choice = input(input_text)
-        choice = ask_int(input_text, valid=indexes)
-
-        if not map_choice:
-            return choice
-        else:
-            return choices[choice - 1][1]
