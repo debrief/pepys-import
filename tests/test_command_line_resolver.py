cb0d8f7280a8e6066cceedb550961906937e6387
BarisSari <sariba@itu.edu.tr>
2020-02-19 11:44:12 +0300

    Implement more tests to cover command line resolver

--- tests/test_command_line_resolver.py
+++ tests/test_command_line_resolver.py
@@ -54,6 +54,35 @@ class CommandLineResolverTestCase(unittest.TestCase):
                 with self.assertRaises(SystemExit):
                     self.resolver.resolve_sensor(self.store, "", "", "")
 
+    def test_resolver_sensor_with_sensor_given(self):
+        with self.store.session_scope():
+            sensor_type = self.store.add_to_sensor_types("SENSOR-TYPE-1")
+            privacy = self.store.add_to_privacies("PRIVACY-1")
+            platform_type = self.store.add_to_platform_types("Warship")
+            nationality = self.store.add_to_nationalities("UK")
+            platform = self.store.get_platform(
+                "PLATFORM-1",
+                nationality=nationality.name,
+                platform_type=platform_type.name,
+                privacy=privacy.name,
+            )
+            all_sensors = self.store.session.query(self.store.db_classes.Sensor).all()
+            platform.get_sensor(
+                data_store=self.store,
+                all_sensors=all_sensors,
+                sensor_name="TEST",
+                sensor_type=sensor_type,
+                privacy=privacy,
+            )
+            sensor = self.resolver.resolve_sensor(
+                data_store=self.store,
+                sensor_name="TEST",
+                sensor_type=sensor_type,
+                privacy=privacy,
+            )
+            self.assertEqual(sensor.name, "TEST")
+            self.assertEqual(sensor.sensor_type_id, sensor_type.sensor_type_id)
+
     def test_resolver_platform(self):
         with self.store.session_scope():
             self.store.add_to_privacies("PRIVACY-1")
@@ -98,6 +127,52 @@ class CommandLineResolverTestCase(unittest.TestCase):
             with StdinAuto(["2", "1", "UK", "1", "Warship", "1", "PRIVACY-1", "."]):
                 with self.assertRaises(SystemExit):
                     self.resolver.resolve_platform(self.store, "", "", "", "")
+
+    def test_resolver_platform_with_values(self):
+        with self.store.session_scope():
+            privacy = self.store.add_to_privacies("PRIVACY-1").name
+            platform_type = self.store.add_to_platform_types("Warship").name
+            nationality = self.store.add_to_nationalities("UK").name
+            with StdinAuto(["2", "1"]):
+                (
+                    platform_name,
+                    platform_type,
+                    nationality,
+                    privacy,
+                ) = self.resolver.resolve_platform(
+                    data_store=self.store,
+                    platform_name="TEST",
+                    platform_type=platform_type,
+                    nationality=nationality,
+                    privacy=privacy,
+                )
+                self.assertEqual(platform_name, "TEST")
+                self.assertEqual(platform_type.name, "Warship")
+                self.assertEqual(nationality.name, "UK")
+                self.assertEqual(privacy.name, "PRIVACY-1")
+
+    def test_resolver_platform_with_platform_given(self):
+        with self.store.session_scope():
+            privacy = self.store.add_to_privacies("PRIVACY-1")
+            platform_type = self.store.add_to_platform_types("Warship")
+            nationality = self.store.add_to_nationalities("UK")
+            self.store.get_platform(
+                "TEST",
+                nationality=nationality.name,
+                platform_type=platform_type.name,
+                privacy=privacy.name,
+            )
+            platform = self.resolver.resolve_platform(
+                data_store=self.store,
+                platform_name="TEST",
+                platform_type=platform_type.name,
+                nationality=nationality.name,
+                privacy=privacy.name,
+            )
+            self.assertEqual(platform.name, "TEST")
+            self.assertEqual(platform.platform_type_id, platform_type.platform_type_id)
+            self.assertEqual(platform.nationality_id, nationality.nationality_id)
+            self.assertEqual(platform.privacy_id, privacy.privacy_id)
 
 
 if __name__ == "__main__":
