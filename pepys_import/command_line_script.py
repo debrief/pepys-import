3015365bee7747d675f6b363c12e469eaa67b2f9
BarisSari <sariba@itu.edu.tr>
2020-02-20 14:20:14 +0300

    Take optional path argument

--- pepys_import/command_line_script.py
+++ pepys_import/command_line_script.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 from pepys_import.file.file_processor import FileProcessor
 from pepys_import.core.store.data_store import DataStore
@@ -9,10 +10,20 @@ FILE_PATH = os.path.abspath(__file__)
 DIRECTORY_PATH = os.path.dirname(FILE_PATH)
 
 
-data_store = DataStore("", "", "", 0, ":memory:", db_type="sqlite")
-data_store.initialise()
+def main(path=DIRECTORY_PATH):
+    data_store = DataStore("", "", "", 0, ":memory:", db_type="sqlite")
+    data_store.initialise()
 
-processor = FileProcessor("descending.db")
-processor.register_importer(ReplayImporter())
-processor.register_importer(NMEAImporter())
-processor.process(DIRECTORY_PATH, data_store, True)
+    processor = FileProcessor("descending.db")
+    processor.register_importer(ReplayImporter())
+    processor.register_importer(NMEAImporter())
+    processor.process(path, data_store, True)
+
+
+if __name__ == "__main__":
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    help = "Please enter a path: (The default value is the directory of the script)"
+    parser.add_argument("--path", help=help, required=False, default=DIRECTORY_PATH)
+    args = parser.parse_args()
+    main(args.path)
