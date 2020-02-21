c0671fd244fd7c5a76782e22efe3580f959142f5
BarisSari <sariba@itu.edu.tr>
2020-02-21 16:46:01 +0300

    Write docstring

--- pepys_import/resolvers/command_line_input.py
+++ pepys_import/resolvers/command_line_input.py
@@ -1,8 +1,19 @@
 from prompt_toolkit import prompt
 
 
-def create_menu(heading, choices, completer=None):
-    input_text = heading + "\n"
+def create_menu(title, choices, completer=None):
+    """
+    A basic function which creates a menu with title and choices.
+
+    :param title: Heading text
+    :type title: String
+    :param choices: Options to choose
+    :type choices: List of strings
+    :param completer: Optional argument that shows possible options while typing.
+    :type completer: :class:`prompt_toolkit.completion.FuzzyCompleter`
+    :return: Entered choice
+    """
+    input_text = title + "\n"
     for index, choice in enumerate(choices, 1):
         input_text += f"   {str(index)}) {choice}\n"
     input_text += f"   .) Cancel import\n > "
