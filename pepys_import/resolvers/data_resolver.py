3a20c2ea4dc2e96fabde87f0e49f408791a01d52
BarisSari <sariba@itu.edu.tr>
2020-02-21 13:47:57 +0300

    Add resolve_datafile abstract method, update docstrings

--- pepys_import/resolvers/data_resolver.py
+++ pepys_import/resolvers/data_resolver.py
@@ -8,20 +8,45 @@ class DataResolver(ABC):
     ):
         """
         Implementation method should return any data necessary to create a platform.
-        Currently: platform_name, platform_type, nationality.
-        Probably host when that is needed
+        Currently: platform_name, platform_type, nationality, privacy.
+
+        :param data_store:
+        :param platform_name:
+        :param platform_type:
+        :param nationality:
+        :param privacy:
+        :return:
         """
 
     @abstractmethod
     def resolve_sensor(self, data_store, sensor_name, sensor_type, privacy):
         """
         Implementation method should return any data necessary to create a sensor.
-        Currently: sensor_name, sensor_type
+        Currently: sensor_name, sensor_type.
+
+        :param data_store:
+        :param sensor_name:
+        :param sensor_type:
+        :param privacy:
+        :return:
         """
 
     @abstractmethod
     def resolve_privacy(self, data_store):
         """
         Implementation method should return any data necessary to create a privacy.
-        Currently: privacy_name
+        Currently: name
+
+        :param data_store:
+        :return:
+        """
+
+    @abstractmethod
+    def resolve_datafile(self, data_store, datafile_type):
+        """
+        Implementation method should return any data necessary to create a datafile.
+        Currently: datafile_type, privacy
+        :param data_store:
+        :param datafile_type:
+        :return:
         """
