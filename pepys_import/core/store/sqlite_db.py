814708e9e209aa01ddf534e0873fafd107867be5
Baris Sari <sariba@itu.edu.tr>
2020-02-25 15:58:06 +0300

    Rename _measurement_files

--- pepys_import/core/store/sqlite_db.py
+++ pepys_import/core/store/sqlite_db.py
@@ -209,7 +209,7 @@ class Participant(BaseSpatiaLite):
 class Datafile(BaseSpatiaLite):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._measurement_files = []
+        self._measurements = []
 
     __tablename__ = "Datafiles"
     table_type = TableTypes.METADATA
@@ -227,14 +227,14 @@ class Datafile(BaseSpatiaLite):
         state = State(
             sensor_id=sensor.sensor_id, time=timestamp, source_id=self.datafile_id
         )
-        self._measurement_files.append(state)
+        self._measurements.append(state)
         return state
 
     def create_contact(self, sensor, timestamp):
         contact = Contact(
             sensor_id=sensor.sensor_id, time=timestamp, source_id=self.datafile_id
         )
-        self._measurement_files.append(contact)
+        self._measurements.append(contact)
         return contact
 
     def create_comment(self, sensor, timestamp, comment, comment_type):
@@ -244,14 +244,14 @@ class Datafile(BaseSpatiaLite):
             comment_type_id=comment_type.comment_type_id,
             source_id=self.datafile_id,
         )
-        self._measurement_files.append(comment)
+        self._measurements.append(comment)
         return comment
 
     def validate(self):
         return True
 
     def commit(self, session):
-        for file in self._measurement_files:
+        for file in self._measurements:
             file.submit(session)
 
     # def verify(self):
