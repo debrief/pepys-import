7645a71b50d6b35199915ee0d918a0d3c59dea0e
Robin Wilson <robin@rtwilson.com>
2020-02-25 12:06:01 +0000

    Updated comments on deployment PS script

--- create_deployment.ps1
+++ create_deployment.ps1
@@ -20,6 +20,7 @@ $url = 'https://www.sqlite.org/2020/sqlite-dll-win64-x64-3310100.zip'
 (New-Object System.Net.WebClient).DownloadFile($url,  "$PWD\sqlite.zip")
 
 # Extract SQLite DLL to Python folder, deliberately overwriting sqlite3.dll provided by Python
+# (note the -Force argument, to force overwriting)
 Expand-Archive -Path sqlite.zip -DestinationPath .\python -Force
 
 Write-Output "INFO: Downloaded and extracted SQLite"
@@ -31,7 +32,7 @@ $url = 'http://www.gaia-gis.it/gaia-sins/windows-bin-NEXTGEN-amd64/mod_spatialit
 # mod_spatialite comes in a 7zip archive, so we need to download 7zip to be able to extract it
 $url = 'http://www.7-zip.org/a/7za920.zip'
 (New-Object System.Net.WebClient).DownloadFile($url,  "$PWD\7zip.zip")
-
+# Put the 7zip exe in the .\7zip folder - we will delete this later
 Expand-Archive -Path 7zip.zip -DestinationPath .\7zip
 
 # Extract the mod_spatialite 7zip file into the lib folder (it creates its own subfolder in there)
@@ -39,7 +40,10 @@ Expand-Archive -Path 7zip.zip -DestinationPath .\7zip
 
 Write-Output "INFO: Downloaded and extracted mod_spatialite"
 
-# Set the Python Path file to tell it where to find modules - including the new pip location and the directory above (with pepys-import in it)
+# Set the Python Path file to tell it where to find modules - including the new pip location and
+# the directory above the python folder (with pepys-import in it). This creates a ._pth file which
+# Python uses as it's *only* source for generating sys.path (ie. it does NOT take into account
+# environment variables such as PYTHONPATH)
 Set-Content -Encoding ascii .\python\python37._pth @"
 python37.zip
 .
@@ -49,7 +53,7 @@ Lib\site-packages
 
 Write-Output "INFO: Set Python pth file"
 
-# Do a standard pip install of the requirements
+# Do a standard pip install of the requirements, not warning us that scripts will be unavailable
 .\python\python.exe -m pip install -r requirements.txt --no-warn-script-location
 
 Write-Output "INFO: Installed Python dependencies"
@@ -62,7 +66,7 @@ Remove-Item get-pip.py
 
 Write-Output "INFO: Finished cleanup"
 
-# Zip up whole folder into a zip-file with the git revision in the name
+# Zip up whole folder into a zip-file with the current git hash in the name
 $gitcommit = git rev-parse --short HEAD
 $output_filename = "pepys-deploy_$gitcommit.zip"
 Compress-Archive -Path .\* -DestinationPath $output_filename
