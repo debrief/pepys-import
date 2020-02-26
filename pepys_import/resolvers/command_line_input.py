bd23c8134247eb7276ce6e712b4c20ced75ff5b9
Baris Sari <sariba@itu.edu.tr>
2020-02-26 10:58:01 +0300

    Include validator

--- pepys_import/resolvers/command_line_input.py
+++ pepys_import/resolvers/command_line_input.py
@@ -1,7 +1,12 @@
 from prompt_toolkit import prompt
+from prompt_toolkit.validation import Validator
 
 
-def create_menu(title, choices, completer=None):
+def is_valid(option):
+    return option == str(1) or option == str(2) or option == "."
+
+
+def create_menu(title, choices, completer=None, validate_method=None):
     """
     A basic function which creates a menu with title and choices.
 
@@ -11,13 +16,23 @@ def create_menu(title, choices, completer=None):
     :type choices: List of strings
     :param completer: Optional argument that shows possible options while typing.
     :type completer: :class:`prompt_toolkit.completion.FuzzyWordCompleter`
+    :param validate_method: Possible validator function
+    :type validate_method: Function
     :return: Entered choice
     :rtype: String
     """
+    validator = None
+    if validate_method is not None:
+        validator = Validator.from_callable(
+            validate_method,
+            error_message="You didn't select a valid option",
+            move_cursor_to_end=True,
+        )
+
     input_text = title + "\n"
     for index, choice in enumerate(choices, 1):
         input_text += f"   {str(index)}) {choice}\n"
     input_text += f"   .) Cancel import\n > "
-    choice = prompt(input_text, completer=completer)
+    choice = prompt(input_text, completer=completer, validator=validator)
 
     return choice
