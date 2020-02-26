2fc21f8edeb034a2a516e5a1c9db77c9e1ec2c3d
Robin Wilson <robin@rtwilson.com>
2020-02-26 17:07:03 +0000

    Added bare-bones outline of tests, based on E-Trac tests

---
+++ tests/test_parse_gpx.py
@@ -0,0 +1,58 @@
+import os
+import unittest
+from sqlite3 import OperationalError
+
+from pepys_import.file.gpx_importer import GPXImporter
+from pepys_import.file.file_processor import FileProcessor
+from pepys_import.core.store.data_store import DataStore
+
+FILE_PATH = os.path.dirname(__file__)
+DATA_PATH = os.path.join(FILE_PATH, "sample_data/track_files/gpx")
+
+
+class GPXTests(unittest.TestCase):
+    def setUp(self):
+        self.store = DataStore("", "", "", 0, ":memory:", db_type="sqlite")
+        self.store.initialise()
+
+    def tearDown(self):
+        pass
+
+    def test_process_e_trac_data(self):
+        processor = FileProcessor()
+        processor.register_importer(GPXImporter())
+
+        # check states empty
+        with self.store.session_scope():
+            # there must be no states at the beginning
+            states = self.store.session.query(self.store.db_classes.State).all()
+            self.assertEqual(len(states), 0)
+
+            # there must be no platforms at the beginning
+            platforms = self.store.session.query(self.store.db_classes.Platform).all()
+            self.assertEqual(len(platforms), 0)
+
+            # there must be no datafiles at the beginning
+            datafiles = self.store.session.query(self.store.db_classes.Datafile).all()
+            self.assertEqual(len(datafiles), 0)
+
+        # parse the folder
+        processor.process(DATA_PATH, self.store, False)
+
+        # check data got created
+        with self.store.session_scope():
+            # there must be states after the import
+            states = self.store.session.query(self.store.db_classes.State).all()
+            self.assertEqual(len(states), 9999)
+
+            # there must be platforms after the import
+            platforms = self.store.session.query(self.store.db_classes.Platform).all()
+            self.assertEqual(len(platforms), 9999)
+
+            # there must be one datafile afterwards
+            datafiles = self.store.session.query(self.store.db_classes.Datafile).all()
+            self.assertEqual(len(datafiles), 9999)
+
+
+if __name__ == "__main__":
+    unittest.main()
