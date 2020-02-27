3b9ca0e765e0c7095d9f924e75f4702654e4c64e
Baris Sari <sariba@itu.edu.tr>
2020-02-27 15:29:47 +0300

    Create one main function which calls the rest of the functions

--- pepys_import/core/validators/basic_validator.py
+++ pepys_import/core/validators/basic_validator.py
@@ -16,7 +16,23 @@ def validate_heading(heading):
     return False
 
 
+def validate_course(course):
+    if 0 <= course <= 360:
+        return True
+    return False
+
+
 def validate_speed(speed):
     if 0 <= speed <= 360:
         return True
     return False
+
+
+def basic_validation(longitude, latitude, heading, course, speed):
+    return (
+        validate_longitude(longitude)
+        and validate_latitude(latitude)
+        and validate_heading(heading)
+        and validate_course(course)
+        and validate_speed(speed)
+    )
--- pepys_import/core/validators/enhanced_validator.py
+++ pepys_import/core/validators/enhanced_validator.py
@@ -22,3 +22,9 @@ def speed_loose_match_with_location(speed, prev_location, current_location):
     :param current_location:
     :return:
     """
+
+
+def enhance_validation(course, heading, speed, prev_location, current_location):
+    return course_heading_loose_match_with_location(
+        course, heading, prev_location, current_location
+    ) and speed_loose_match_with_location(speed, prev_location, current_location)
