b63f4a98cd56799cfb0b7c9aa0cc100c9ecb2467
Baris Sari <sariba@itu.edu.tr>
2020-02-27 15:19:10 +0300

    Create placeholder functions

---
+++ pepys_import/core/validators/enhanced_validator.py
@@ -0,0 +1,24 @@
+"""
+Enhanced tests actually serve to verify the lat/long, in addition to the course/speed/heading
+"""
+
+
+def course_heading_loose_match_with_location(
+    course, heading, prev_location, current_location
+):
+    """
+    :param course:
+    :param heading:
+    :param prev_location:
+    :param current_location:
+    :return:
+    """
+
+
+def speed_loose_match_with_location(speed, prev_location, current_location):
+    """
+    :param speed:
+    :param prev_location:
+    :param current_location:
+    :return:
+    """
