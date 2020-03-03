241e6e091adb1389b5b489918315a0555f31d9f6
Baris Sari <sariba@itu.edu.tr>
2020-03-03 10:22:29 +0300

    Change name of main function

--- pepys_import/core/validators/enhanced_validator.py
+++ pepys_import/core/validators/enhanced_validator.py
@@ -24,7 +24,7 @@ def speed_loose_match_with_location(speed, prev_location, current_location):
     """
 
 
-def enhance_validation(course, heading, speed, prev_location, current_location):
+def enhanced_validation(course, heading, speed, prev_location, current_location):
     return course_heading_loose_match_with_location(
         course, heading, prev_location, current_location
     ) and speed_loose_match_with_location(speed, prev_location, current_location)
