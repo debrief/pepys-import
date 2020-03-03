31e17b4c5b23fb4542184f64128c976a78513109
Baris Sari <sariba@itu.edu.tr>
2020-03-03 20:49:56 +0300

    Convert validator to a class

--- pepys_import/core/validators/enhanced_validator.py
+++ pepys_import/core/validators/enhanced_validator.py
@@ -2,43 +2,24 @@
 Enhanced tests actually serve to verify the lat/long, in addition to the course/speed/heading
 """
 
-MESSAGE = f"Enhanced Validation Error"
 
+class EnhancedValidator:
+    def __init__(self, measurement_object, errors, message, prev_location):
+        self.error_message = message + f" - Enhanced Validation Error"
+        self.errors = errors
+        self.heading = measurement_object.heading
+        self.course = measurement_object.course
+        self.prev_location = measurement_object.prev_location
 
-def course_heading_loose_match_with_location(
-    course, heading, prev_location, current_location, errors
-):
-    # TODO: calculate current location, put prev and curr location to ST_Distance,
-    #  compare it with course and heading
-    """
-    :param course:
-    :param heading:
-    :param prev_location:
-    :param current_location:
-    :param errors:
-    :return:
-    """
+        self.course_heading_loose_match_with_location()
+        self.speed_loose_match_with_location()
 
+    def course_heading_loose_match_with_location(self):
+        # TODO: calculate current location, put prev and curr location to ST_Distance,
+        #  compare it with course and heading
+        """"""
 
-def speed_loose_match_with_location(speed, prev_location, current_location, errors):
-    # TODO: calculate current location, put prev and curr location to ST_Distance,
-    #  compare it with speed
-    """
-    :param speed:
-    :param prev_location:
-    :param current_location:
-    :param errors:
-    :return:
-    """
-
-
-def enhanced_validation(measurement_object, prev_location, errors):
-    return course_heading_loose_match_with_location(
-        measurement_object.course,
-        measurement_object.heading,
-        prev_location,
-        measurement_object.location,
-        errors,
-    ) and speed_loose_match_with_location(
-        measurement_object.speed, prev_location, measurement_object.location, errors
-    )
+    def speed_loose_match_with_location(self):
+        # TODO: calculate current location, put prev and curr location to ST_Distance,
+        #  compare it with speed
+        """"""
