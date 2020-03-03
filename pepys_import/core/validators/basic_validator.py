0f78488b2b355b442e7a870e6a201b9f10e1779f
Baris Sari <sariba@itu.edu.tr>
2020-03-03 17:09:34 +0300

    Add errors as an argument

--- pepys_import/core/validators/basic_validator.py
+++ pepys_import/core/validators/basic_validator.py
@@ -1,38 +1,45 @@
-def validate_longitude(longitude):
+MESSAGE = f"Basic Validation Error"
+
+
+def validate_longitude(longitude, errors):
     if longitude and -90 <= longitude <= 90:
         return True
+    errors.append({MESSAGE: "Longitude is not between -90 and 90 degrees!"})
     return False
 
 
-def validate_latitude(latitude):
+def validate_latitude(latitude, errors):
     if latitude and -180 <= latitude <= 180:
         return True
+    errors.append({MESSAGE: "Latitude is not between -180 and 180 degrees!"})
     return False
 
 
-def validate_heading(heading):
+def validate_heading(heading, errors):
     if heading and 0 <= heading <= 360:
         return True
+    errors.append({MESSAGE: "Heading is not between 0 and 360 degrees!"})
     return False
 
 
-def validate_course(course):
+def validate_course(course, errors):
     if course and 0 <= course <= 360:
         return True
+    errors.append({MESSAGE: "Course is not between 0 and 360 degrees!"})
     return False
 
 
-def basic_validation(measurement_object):
+def basic_validation(measurement_object, errors):
     if measurement_object.location is not None:
         longitude, latitude = measurement_object.location[6:-1].split()
         return (
-            validate_longitude(float(longitude))
-            and validate_latitude(float(latitude))
-            and validate_heading(measurement_object.heading)
-            and validate_course(measurement_object.course)
+            validate_longitude(float(longitude), errors)
+            and validate_latitude(float(latitude), errors)
+            and validate_heading(measurement_object.heading, errors)
+            and validate_course(measurement_object.course, errors)
         )
-    return validate_heading(measurement_object.heading) and validate_course(
-        measurement_object.course
+    return validate_heading(measurement_object.heading, errors) and validate_course(
+        measurement_object.course, errors
     )
 
 
--- pepys_import/core/validators/enhanced_validator.py
+++ pepys_import/core/validators/enhanced_validator.py
@@ -2,29 +2,39 @@
 Enhanced tests actually serve to verify the lat/long, in addition to the course/speed/heading
 """
 
+MESSAGE = f"Enhanced Validation Error"
+
 
 def course_heading_loose_match_with_location(
-    course, heading, prev_location, current_location
+    course, heading, prev_location, current_location, errors
 ):
     """
     :param course:
     :param heading:
     :param prev_location:
     :param current_location:
+    :param errors:
     :return:
     """
 
 
-def speed_loose_match_with_location(speed, prev_location, current_location):
+def speed_loose_match_with_location(speed, prev_location, current_location, errors):
     """
     :param speed:
     :param prev_location:
     :param current_location:
+    :param errors:
     :return:
     """
 
 
-def enhanced_validation(course, heading, speed, prev_location, current_location):
+def enhanced_validation(measurement_object, prev_location, errors):
     return course_heading_loose_match_with_location(
-        course, heading, prev_location, current_location
-    ) and speed_loose_match_with_location(speed, prev_location, current_location)
+        measurement_object.course,
+        measurement_object.heading,
+        prev_location,
+        measurement_object.location,
+        errors,
+    ) and speed_loose_match_with_location(
+        measurement_object.speed, prev_location, measurement_object.location, errors
+    )
