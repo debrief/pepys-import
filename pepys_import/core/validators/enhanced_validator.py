8f9cf034b0d37b70756c7d1fc69466c0d6871897
Baris Sari <sariba@itu.edu.tr>
2020-03-04 12:23:41 +0300

    Use util functions to match distance and bearing between two points

--- pepys_import/core/validators/enhanced_validator.py
+++ pepys_import/core/validators/enhanced_validator.py
@@ -1,43 +1,44 @@
-"""
-Enhanced tests actually serve to verify the lat/long, in addition to the course/speed/heading
-"""
-from geoalchemy2 import func, WKTElement
-from sqlalchemy import select
+from pepys_import.utils.unit_utils import (
+    bearing_between_two_points,
+    distance_between_two_points_haversine,
+)
 
 
 class EnhancedValidator:
-    def __init__(self, measurement_object, errors, message, prev_location, engine):
+    """Enhanced validator serve to verify the lat/long, in addition to the course/speed/heading"""
+
+    def __init__(self, measurement_object, errors, message, prev_location):
         self.error_message = message + f" - Enhanced Validation Error"
         self.errors = errors
         self.location = measurement_object.location
+        self.speed = measurement_object.speed
         self.heading = measurement_object.heading
         self.course = measurement_object.course
         self.prev_location = prev_location
-        self.engine = engine
 
         self.course_heading_loose_match_with_location()
         self.speed_loose_match_with_location()
 
     def course_heading_loose_match_with_location(self):
-        # TODO: calculate current location, put prev and curr location to ST_Distance,
-        #  compare it with course and heading
-        with self.engine.connect() as conn:
-            query = select(
-                [func.ST_Distance(self.location, WKTElement(self.prev_location))]
-            )
-            result = conn.execute(query)
-            distance = result.fetchone()
-
-        print(distance)
+        bearing = bearing_between_two_points(self.location, self.prev_location)
+        print(bearing)
+        if bearing <= 90:
+            return True
+        self.errors.append(
+            {self.error_message: f"Bearing ({bearing}) is more than 90 degrees!"}
+        )
 
     def speed_loose_match_with_location(self):
-        # TODO: calculate current location, put prev and curr location to ST_Distance,
-        #  compare it with speed
-        with self.engine.connect() as conn:
-            query = select(
-                [func.ST_Distance(self.location, WKTElement(self.prev_location))]
-            )
-            result = conn.execute(query)
-            distance = result.fetchone()
-
-        print(distance)
+        distance = distance_between_two_points_haversine(
+            self.location, self.prev_location
+        )
+        print(distance, self.speed * 10)
+        if distance <= self.speed * 10:
+            return True
+        self.errors.append(
+            {
+                self.error_message: f"Distance ({distance} km) is more than speed * 10 ("
+                f"{self.speed * 10})!"
+            }
+        )
+        return False
