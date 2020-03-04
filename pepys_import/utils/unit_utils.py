7494aeb6c733566bc8d56b868324c1bf8abb4e34
Baris Sari <sariba@itu.edu.tr>
2020-03-04 12:12:52 +0300

    Implement bearing_between_two_points, add extra function to extract points in radians

--- pepys_import/utils/unit_utils.py
+++ pepys_import/utils/unit_utils.py
@@ -1,4 +1,4 @@
-from math import radians, cos, sin, asin, sqrt
+from math import radians, cos, sin, asin, sqrt, atan2, degrees
 
 from pepys_import.core.formats import unit_registry
 
@@ -58,6 +58,36 @@ def convert_string_location_to_degrees(first_location):
     return float(longitude), float(latitude)
 
 
+def extract_points(location):
+    # convert string point to float point
+    if isinstance(location, str):
+        location = convert_string_location_to_degrees(location)
+    longitude, latitude = location
+    # convert decimal degrees to radians and return
+    return radians(longitude), radians(latitude)
+
+
+def bearing_between_two_points(first_location, second_location):
+    """
+    Calculate the bearing(angle) between two points on the earth (specified in decimal degrees).
+
+    :param first_location: First location point
+    :param second_location: Second location point
+    :return: angle between two points in degrees
+    """
+    longitude_1, latitude_1 = extract_points(first_location)
+    longitude_2, latitude_2 = extract_points(second_location)
+    diff_longitude = longitude_2 - longitude_1
+
+    y = sin(diff_longitude) * cos(latitude_2)
+    x = cos(latitude_1) * sin(latitude_2) - sin(latitude_1) * cos(latitude_2) * cos(
+        diff_longitude
+    )
+    bearing = degrees((atan2(y, x)))
+    bearing = 360 - ((bearing + 360) % 360)
+    return bearing
+
+
 def distance_between_two_points_haversine(first_location, second_location):
     """
     Calculate the great circle distance between two points on the earth (specified in decimal degrees).
@@ -66,24 +96,15 @@ def distance_between_two_points_haversine(first_location, second_location):
     :param second_location: Second location point
     :return: distance in kilometers
     """
-    # convert string point to float point
-    if isinstance(first_location, str):
-        first_location = convert_string_location_to_degrees(first_location)
-    if isinstance(second_location, str):
-        second_location = convert_string_location_to_degrees(second_location)
-    longitude_1, latitude_1 = first_location
-    longitude_2, latitude_2 = second_location
-    # convert decimal degrees to radians
-    lon1, lat1, lon2, lat2 = map(
-        radians, [longitude_1, latitude_1, longitude_2, latitude_2]
-    )
+    longitude_1, latitude_1 = extract_points(first_location)
+    longitude_2, latitude_2 = extract_points(second_location)
 
     # haversine formula
-    diff_longitude = lon2 - lon1
-    diff_latitude = lat2 - lat1
+    diff_longitude = longitude_2 - longitude_1
+    diff_latitude = latitude_2 - latitude_1
     a = (
         sin(diff_latitude / 2) ** 2
-        + cos(lat1) * cos(lat2) * sin(diff_longitude / 2) ** 2
+        + cos(latitude_1) * cos(latitude_2) * sin(diff_longitude / 2) ** 2
     )
     c = 2 * asin(sqrt(a))
     radius = 6371  # Radius of earth in kilometers. Use 3956 for miles
