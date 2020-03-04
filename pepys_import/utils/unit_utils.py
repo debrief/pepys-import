e1d4d3f9c773f4a66924d087a709dec62bded4b3
Baris Sari <sariba@itu.edu.tr>
2020-03-04 11:33:13 +0300

    Implement new functions for calculating the distance between two points

--- pepys_import/utils/unit_utils.py
+++ pepys_import/utils/unit_utils.py
@@ -1,3 +1,5 @@
+from math import radians, cos, sin, asin, sqrt
+
 from pepys_import.core.formats import unit_registry
 
 
@@ -49,3 +51,40 @@ def convert_speed(speed, line_number):
         .magnitude
     )
     return speed
+
+
+def convert_string_location_to_degrees(first_location):
+    longitude, latitude = first_location[6:-1].split()
+    return float(longitude), float(latitude)
+
+
+def distance_between_two_points_haversine(first_location, second_location):
+    """
+    Calculate the great circle distance between two points on the earth (specified in decimal degrees).
+
+    :param first_location: First location point
+    :param second_location: Second location point
+    :return: distance in kilometers
+    """
+    # convert string point to float point
+    if isinstance(first_location, str):
+        first_location = convert_string_location_to_degrees(first_location)
+    if isinstance(second_location, str):
+        second_location = convert_string_location_to_degrees(second_location)
+    longitude_1, latitude_1 = first_location
+    longitude_2, latitude_2 = second_location
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(
+        radians, [longitude_1, latitude_1, longitude_2, latitude_2]
+    )
+
+    # haversine formula
+    diff_longitude = lon2 - lon1
+    diff_latitude = lat2 - lat1
+    a = (
+        sin(diff_latitude / 2) ** 2
+        + cos(lat1) * cos(lat2) * sin(diff_longitude / 2) ** 2
+    )
+    c = 2 * asin(sqrt(a))
+    radius = 6371  # Radius of earth in kilometers. Use 3956 for miles
+    return c * radius
