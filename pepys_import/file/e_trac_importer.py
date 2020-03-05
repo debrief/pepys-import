4130e189b463bbbc5aa13612bc4f506c5c6c6b52
Robin Wilson <robin@rtwilson.com>
2020-03-05 11:16:17 +0000

    First go at E-Trac parser with extraction highlighting.
     - All tests pass
     - Not much actual `record`ing going on yet, but everything else working

--- pepys_import/file/e_trac_importer.py
+++ pepys_import/file/e_trac_importer.py
@@ -2,6 +2,7 @@ from .importer import Importer
 from datetime import datetime
 from pepys_import.core.formats import unit_registry
 from pepys_import.utils.unit_utils import convert_absolute_angle, convert_speed
+from pepys_import.file.highlighter.support.combine import combine_tokens
 
 
 class ETracImporter(Importer):
@@ -25,15 +26,15 @@ class ETracImporter(Importer):
     def can_load_this_file(self, file_contents):
         return True
 
-    def load_this_file(self, data_store, path, file_contents, datafile):
+    def load_this_file(self, data_store, path, file_object, datafile):
         print("E-trac parser working on ", path)
 
-        for line_number, line in enumerate(file_contents, 1):
+        for line_number, line in enumerate(file_object.lines(), 1):
             # Skip the header
             if line_number == 1:
                 continue
 
-            tokens = line.split(",")
+            tokens = line.tokens(line.CSV_DELIM, ",")
             if len(tokens) <= 1:
                 # the last line may be empty, don't worry
                 continue
@@ -52,23 +53,26 @@ class ETracImporter(Importer):
             heading_token = tokens[8]
             speed_token = tokens[6]
             comp_name_token = tokens[18]
-            vessel_name = self.name_for(comp_name_token)
+            vessel_name = self.name_for(comp_name_token.text)
 
-            if len(date_token) != 12:
-                print(len(date_token))
+            if len(date_token.text) != 10:
+                print(len(date_token.text))
                 print(
-                    f"Line {line_number}. Error in Date format {date_token}. Should be 10 figure data"
+                    f"Line {line_number}. Error in Date format '{date_token.text}'. Should be 10 figure data"
                 )
                 continue
 
             # Times always in Zulu/GMT
-            if len(time_token) != 8:
+            if len(time_token.text) != 8:
                 print(
-                    f"Line {line_number}. Error in Date format {time_token}. Should be HH:mm:ss"
+                    f"Line {line_number}. Error in Date format {time_token.text}. Should be HH:mm:ss"
                 )
                 continue
 
-            timestamp = self.parse_timestamp(date_token, time_token)
+            timestamp = self.parse_timestamp(date_token.text, time_token.text)
+
+            timestamp_combined_token = combine_tokens(date_token, time_token)
+            timestamp_combined_token.record(self.name, "timestamp", timestamp, "n/a")
 
             # and finally store it
             platform = data_store.get_platform(
@@ -88,14 +92,18 @@ class ETracImporter(Importer):
             state = datafile.create_state(sensor, timestamp)
             state.privacy = privacy.privacy_id
 
-            state.location = f"POINT({long_degrees_token} {lat_degrees_token})"
+            state.location = (
+                f"POINT({long_degrees_token.text} {lat_degrees_token.text})"
+            )
 
+            # TODO: Depth is currently set to a constant value of 0.0
+            # Do we need to parse this from the file?
             state.elevation = -1 * self.depth
 
-            heading = convert_absolute_angle(heading_token, line_number)
+            heading = convert_absolute_angle(heading_token.text, line_number)
             state.heading = heading.to(unit_registry.radians).magnitude
 
-            speed = convert_speed(speed_token, line_number)
+            speed = convert_speed(speed_token.text, line_number)
             state.speed = speed
 
     @staticmethod
