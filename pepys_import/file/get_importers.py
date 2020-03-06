ad6e4f5741d59a05915e505e2d21c888fa318d80
Ian Mayo <ian@planetmayo.com>
2020-03-06 13:06:29 +0000

    introduce comment importer

--- pepys_import/file/get_importers.py
+++ pepys_import/file/get_importers.py
@@ -15,6 +15,7 @@ def get_importers():
 
     STATIC_IMPORTERS = [
         ReplayImporter(),
+        ReplayCommentImporter(),
         NMEAImporter(),
         GPXImporter(),
         ETracImporter(),
