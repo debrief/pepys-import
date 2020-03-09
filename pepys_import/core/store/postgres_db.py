45547373e3724a0d876e5b9f489b68683bd763e3
Robin Wilson <robin@rtwilson.com>
2020-03-09 10:13:22 +0000

    Commented out EnhancedValidator as it has a bug at the moment

--- pepys_import/core/store/postgres_db.py
+++ pepys_import/core/store/postgres_db.py
@@ -288,7 +288,9 @@ class Datafile(BasePostGIS):
         elif validation_level == validation_constants.ENHANCED_LEVEL:
             for measurement in self.measurements[parser]:
                 BasicValidator(measurement, errors, parser)
-                EnhancedValidator(measurement, errors, parser)
+                # TODO: Commented out at the moment as there is a bug in the validator
+                # Need to bring this back in once that is fixed.
+                # EnhancedValidator(measurement, errors, parser)
             if not errors:
                 return True
             return False
