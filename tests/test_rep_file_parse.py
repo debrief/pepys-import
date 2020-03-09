208d2f97360c90a63d7a5a99c7f4ed36fd0fcf99
Robin Wilson <robin@rtwilson.com>
2020-03-09 09:35:31 +0000

    Fix rep line parsing tests to use new test function to create highlighter objects

--- tests/test_rep_file_parse.py
+++ tests/test_rep_file_parse.py
@@ -3,6 +3,7 @@ import datetime
 
 from pepys_import.core.formats.location import Location
 from pepys_import.core.formats.rep_line import REPLine
+from pepys_import.file.highlighter.support.test_utils import create_test_line_object
 from contextlib import redirect_stdout
 from io import StringIO
 
@@ -16,7 +17,9 @@ class BasicTests(unittest.TestCase):
         # long date
         rep_line = REPLine(
             1,
-            "19951212 120800 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  NaN",
+            create_test_line_object(
+                "19951212 120800 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  NaN"
+            ),
             " ",
         )
         self.assertTrue(rep_line.parse(self.error, self.message))
@@ -25,7 +28,9 @@ class BasicTests(unittest.TestCase):
         # long time
         rep_line = REPLine(
             1,
-            "951212 120800.555 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 ",
+            create_test_line_object(
+                "951212 120800.555 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 "
+            ),
             " ",
         )
         self.assertTrue(rep_line.parse(self.error, self.message))
@@ -33,13 +38,19 @@ class BasicTests(unittest.TestCase):
 
     def test_error_reports(self):
         # too few fields
-        rep_line = REPLine(1, " 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 ", " ")
+        rep_line = REPLine(
+            1,
+            create_test_line_object(" 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 "),
+            " ",
+        )
         self.assertFalse(rep_line.parse(self.error, self.message))
 
         # wrong length date
         rep_line = REPLine(
             1,
-            "12 120800 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 ",
+            create_test_line_object(
+                "12 120800 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 "
+            ),
             " ",
         )
         self.assertFalse(rep_line.parse(self.error, self.message))
@@ -47,7 +58,9 @@ class BasicTests(unittest.TestCase):
         # wrong length time
         rep_line = REPLine(
             1,
-            "951212 12 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 ",
+            create_test_line_object(
+                "951212 12 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 "
+            ),
             " ",
         )
         self.assertFalse(rep_line.parse(self.error, self.message))
@@ -55,7 +68,9 @@ class BasicTests(unittest.TestCase):
         # wrong length symbology
         rep_line = REPLine(
             1,
-            "951212 120800 SUBJECT VCC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 ",
+            create_test_line_object(
+                "951212 120800 SUBJECT VCC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 "
+            ),
             " ",
         )
         self.assertFalse(rep_line.parse(self.error, self.message))
@@ -63,14 +78,18 @@ class BasicTests(unittest.TestCase):
         # wrong symbology
         rep_line = REPLine(
             1,
-            "951212 120800 SUBJECT VC[VC[VC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 ",
+            create_test_line_object(
+                "951212 120800 SUBJECT VC[VC[VC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 "
+            ),
             " ",
         )
         self.assertFalse(rep_line.parse(self.error, self.message))
         # bad latitude
         rep_line = REPLine(
             1,
-            "951212 120800 SUBJECT VC 6A 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 ",
+            create_test_line_object(
+                "951212 120800 SUBJECT VC 6A 23 40.25 N 000 01 25.86 E 109.08  6.00  0.00 "
+            ),
             " ",
         )
         self.assertFalse(rep_line.parse(self.error, self.message))
@@ -78,7 +97,9 @@ class BasicTests(unittest.TestCase):
         # bad longitude
         rep_line = REPLine(
             1,
-            "951212 120800 SUBJECT VC 60 23 40.25 N 00A 01 2B.86 E 109.08  6.00  0.00 ",
+            create_test_line_object(
+                "951212 120800 SUBJECT VC 60 23 40.25 N 00A 01 2B.86 E 109.08  6.00  0.00 "
+            ),
             " ",
         )
         self.assertFalse(rep_line.parse(self.error, self.message))
@@ -86,7 +107,9 @@ class BasicTests(unittest.TestCase):
         # bad heading
         rep_line = REPLine(
             1,
-            "951212 120800 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 10b.08  6.00  0.00 ",
+            create_test_line_object(
+                "951212 120800 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 10b.08  6.00  0.00 "
+            ),
             " ",
         )
         self.assertFalse(rep_line.parse(self.error, self.message))
@@ -94,7 +117,9 @@ class BasicTests(unittest.TestCase):
         # bad speed
         rep_line = REPLine(
             1,
-            "951212 120800 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.b0  0.00 ",
+            create_test_line_object(
+                "951212 120800 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.b0  0.00 "
+            ),
             " ",
         )
         self.assertFalse(rep_line.parse(self.error, self.message))
@@ -102,7 +127,9 @@ class BasicTests(unittest.TestCase):
         # bad depth
         rep_line = REPLine(
             1,
-            "951212 120800 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.c0 ",
+            create_test_line_object(
+                "951212 120800 SUBJECT VC 60 23 40.25 N 000 01 25.86 E 109.08  6.00  0.c0 "
+            ),
             " ",
         )
         self.assertFalse(rep_line.parse(self.error, self.message))
@@ -110,8 +137,9 @@ class BasicTests(unittest.TestCase):
     def test_line_ok(self):
         rep_line = REPLine(
             line_number=1,
-            line="100112\t120800\tSUBJECT\tVC\t60\t23\t40.25\tS\t000\t01\t25.86\tE\t"
-            "109.08\t6.00\t0.00\tLabel",
+            line=create_test_line_object(
+                "100112\t120800\tSUBJECT\tVC\t60\t23\t40.25\tS\t000\t01\t25.86\tE\t109.08\t6.00\t0.00\tLabel"
+            ),
             separator="\t",
         )
         self.assertTrue(rep_line.parse(self.error, self.message))
