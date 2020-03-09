ab454dad145d060fcd094de4a037a24b8c9e236c
Robin Wilson <robin@rtwilson.com>
2020-03-09 09:36:43 +0000

    Add docs

--- pepys_import/file/highlighter/support/test_utils.py
+++ pepys_import/file/highlighter/support/test_utils.py
@@ -5,12 +5,22 @@ from pepys_import.file.highlighter.support.line import Line
 
 
 def create_test_line_object(line_str):
+    """
+    Create a Line object for the given string, with all of the various member
+    variables set properly, so it can be passed to something expecting a line
+    and work.
+
+    Used for tests, particularly of the REP line parser
+    """
+    # Create a highlighted file object but with no filename attached
     test_hf = HighlightedFile(None)
 
+    # Fill the chars array manually
     for char in line_str:
         char_obj = Char(char)
         test_hf.chars.append(char_obj)
 
+    # Create a line object ready to return
     line_span = (0, len(line_str))
     subToken = SubToken(line_span, line_str, 0, test_hf.chars)
     new_line = Line([subToken], test_hf)
