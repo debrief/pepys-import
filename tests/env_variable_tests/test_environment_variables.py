de81ed7d3ff13949a424955389949de837deb3ba
Baris Sari <sariba@itu.edu.tr>
2020-03-11 17:50:13 +0300

    Implement test_bad_pepys_local_parsers_path

--- tests/env_variable_tests/test_environment_variables.py
+++ tests/env_variable_tests/test_environment_variables.py
@@ -1,12 +1,15 @@
 import unittest
 import os
 
+from contextlib import redirect_stdout
+from io import StringIO
 from unittest.mock import patch
 
 from pepys_import.file.file_processor import FileProcessor
 
 DIRECTORY_PATH = os.path.dirname(__file__)
 TEST_IMPORTER_PATH = os.path.join(DIRECTORY_PATH, "test_local_imports")
+BAD_IMPORTER_PATH = os.path.join(DIRECTORY_PATH, "bad_path")
 
 
 class EnvironmentVariablesTestCase(unittest.TestCase):
@@ -18,6 +21,22 @@ class EnvironmentVariablesTestCase(unittest.TestCase):
         file_processor = FileProcessor()
         assert len(file_processor.importers) == 1
         assert file_processor.importers[0].name == "Test Importer"
+
+    @patch.dict(os.environ, {"PEPYS_LOCAL_PARSERS": BAD_IMPORTER_PATH})
+    def test_bad_pepys_local_parsers_path(self):
+        value = os.getenv("PEPYS_LOCAL_PARSERS")
+        assert value == BAD_IMPORTER_PATH
+
+        temp_output = StringIO()
+        with redirect_stdout(temp_output):
+            file_processor = FileProcessor()
+        output = temp_output.getvalue()
+
+        assert len(file_processor.importers) == 0
+        assert (
+            output
+            == f"No such file or directory: {value}. Only core parsers are going to work.\n"
+        )
 
     @patch.dict(os.environ, {"PEPYS_ARCHIVE_LOCATION": "./test_output/"})
     def test_pepys_archive_location(self):
