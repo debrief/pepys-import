43f40384957261eef240b1f38a9824902f3ad733
Ian Mayo <ian@planetmayo.com>
2020-03-12 15:42:39 +0000

    Develop contact importer

--- pepys_import/file/replay_contact_importer.py
+++ pepys_import/file/replay_contact_importer.py
@@ -3,7 +3,9 @@ from pepys_import.core.validators import constants
 from pepys_import.core.formats.rep_line import parse_timestamp
 from pepys_import.file.highlighter.support.combine import combine_tokens
 from pepys_import.utils.unit_utils import convert_absolute_angle
+from pepys_import.utils.unit_utils import convert_distance
 from pepys_import.core.formats import unit_registry
+from pepys_import.core.formats.location import Location
 
 
 class ReplayContactImporter(Importer):
@@ -33,6 +35,10 @@ class ReplayContactImporter(Importer):
     def _load_this_file(self, data_store, path, file_object, datafile):
         for line_number, line in enumerate(file_object.lines(), 1):
             if line.text.startswith(";"):
+                # we'll be using this value to determine if we have location
+                lat_degrees_token = None
+                ambig_bearing_token = None
+                freq_token = None
                 if line.text.startswith(";SENSOR:"):
                     # ok for for it
                     tokens = line.tokens()
@@ -50,13 +56,13 @@ class ReplayContactImporter(Importer):
                     date_token = tokens[1]
                     time_token = tokens[2]
                     vessel_name_token = tokens[3]
-                    symbology = tokens[4]
+                    # symbology = tokens[4]
 
                     # the next one may be degs, or it may be NULL
                     next_token = tokens[5]
                     if next_token.text.upper() == "NULL":
                         location = None
-                        token_ctr = 6
+                        token_ctr = 5
                     else:
                         token_ctr = 13
                         lat_degrees_token = tokens[5]
@@ -68,69 +74,123 @@ class ReplayContactImporter(Importer):
                         long_secs_token = tokens[11]
                         long_hemi_token = tokens[12]
 
-                        tatitude = Location(
-                            lat_degrees_token.text,
-                            lat_mins_token.text,
-                            lat_secs_token.text,
-                            lat_hemi_token.text,
-                            errors,
-                            error_type,
-                        )
-                        combine_tokens(
-                            lat_degrees_token,
-                            lat_mins_token,
-                            lat_secs_token,
-                            lat_hemi_token,
-                        ).record(self.importer_name, "latitude", self.latitude, "DMS")
-
-                        longitude = Location(
-                            long_degrees_token.text,
-                            long_mins_token.text,
-                            long_secs_token.text,
-                            long_hemi_token.text,
-                            errors,
-                            error_type,
-                        )
-                        combine_tokens(
-                            long_degrees_token,
-                            long_mins_token,
-                            long_secs_token,
-                            long_hemi_token,
-                        ).record(self.importer_name, "longitude", self.longitude, "DMS")
-
-                        location = (
-                            f"POINT({self.longitude.as_degrees()} "
-                            f"{self.latitude.as_degrees()})"
-                        )
-
                     token_ctr += 1
                     bearing_token = tokens[token_ctr]
-                    if bearing_token.text.upper() == "NULL":
-                        bearing = 0
-                    else:
-                        bearing = convert_absolute_angle(
-                            bearing_token.text, line, self.errors, self.error_type
-                        )
 
                     token_ctr += 1
                     range_token = tokens[token_ctr]
-                    if range_token.text.upper() == "NULL":
-                        range = 0
-                    else:
-                        range = convert_distance(
-                            range_token.text,
-                            unit_registry.yard,
-                            line,
-                            self.errors,
-                            self.error_type,
-                        )
 
                     token_ctr += 1
                     sensor_name = tokens[token_ctr]
+
+                    sensor_name.record(self.name, "sensor", sensor_name.text, "N/A")
+
                     token_ctr += 1
-                    label = tokens[token_ctr:]
+                    # label = tokens[token_ctr:]
+
+                elif line.text.startswith(";SENSOR2:"):
+                    # ok for for it
+                    tokens = line.tokens()
+
+                    if len(tokens) < 5:
+                        self.errors.append(
+                            {
+                                self.error_type: f"Error on line {line_number}. "
+                                f"Not enough tokens: {line.text}"
+                            }
+                        )
+                        continue
+
+                    # separate token strings
+                    date_token = tokens[1]
+                    time_token = tokens[2]
+                    vessel_name_token = tokens[3]
+                    # symbology = tokens[4]
+
+                    # the next one may be degs, or it may be NULL
+                    next_token = tokens[5]
+                    if next_token.text.upper() == "NULL":
+                        location = None
+                        token_ctr = 5
+                    else:
+                        token_ctr = 13
+                        lat_degrees_token = tokens[5]
+                        lat_mins_token = tokens[6]
+                        lat_secs_token = tokens[7]
+                        lat_hemi_token = tokens[8]
+                        long_degrees_token = tokens[9]
+                        long_mins_token = tokens[10]
+                        long_secs_token = tokens[11]
+                        long_hemi_token = tokens[12]
+
+                    token_ctr += 1
+                    bearing_token = tokens[token_ctr]
+
+                    token_ctr += 1
+                    ambig_bearing_token = tokens[token_ctr]
+
+                    token_ctr += 1
+                    freq_token = tokens[token_ctr]
+
+                    token_ctr += 1
+                    range_token = tokens[token_ctr]
+
+                    token_ctr += 1
+                    sensor_name = tokens[token_ctr]
+
+                    sensor_name.record(self.name, "sensor", sensor_name.text, "N/A")
+
+                    token_ctr += 1
+                    # label = tokens[token_ctr:]
+
                 else:
                     continue
+
+                # do we have location?
+                if lat_degrees_token is None:
+                    location = None
+                else:
+                    latitude = Location(
+                        lat_degrees_token.text,
+                        lat_mins_token.text,
+                        lat_secs_token.text,
+                        lat_hemi_token.text,
+                        self.errors,
+                        self.error_type,
+                    )
+                    combine_tokens(
+                        lat_degrees_token,
+                        lat_mins_token,
+                        lat_secs_token,
+                        lat_hemi_token,
+                    ).record(self.importer_name, "latitude", self.latitude, "DMS")
+
+                    longitude = Location(
+                        long_degrees_token.text,
+                        long_mins_token.text,
+                        long_secs_token.text,
+                        long_hemi_token.text,
+                        self.errors,
+                        self.error_type,
+                    )
+                    combine_tokens(
+                        long_degrees_token,
+                        long_mins_token,
+                        long_secs_token,
+                        long_hemi_token,
+                    ).record(self.importer_name, "longitude", self.longitude, "DMS")
+
+                    location = (
+                        f"POINT({longitude.as_degrees()} " f"{latitude.as_degrees()})"
+                    )
+
+                if bearing_token.text.upper() == "NULL":
+                    bearing = 0
+                else:
+                    bearing = convert_absolute_angle(
+                        bearing_token.text, line, self.errors, self.error_type
+                    )
+                bearing_token.record(self.name, "bearing", bearing, "degs")
 
                 privacy = data_store.missing_data_resolver.resolve_privacy(data_store)
                 platform = data_store.get_platform(
@@ -142,31 +202,57 @@ class ReplayContactImporter(Importer):
                 vessel_name_token.record(
                     self.name, "vessel name", vessel_name_token.text, "n/a"
                 )
-                sensor_type = data_store.add_to_sensor_types("Human")
+                sensor_type = data_store.add_to_sensor_types(sensor_name.text)
                 sensor = platform.get_sensor(
                     data_store=data_store,
                     sensor_name=platform.name,
                     sensor_type=sensor_type,
                     privacy=privacy.name,
                 )
-                comment_type = data_store.add_to_comment_types(comment_type)
 
                 timestamp = parse_timestamp(date_token.text, time_token.text)
                 combine_tokens(date_token, time_token).record(
                     self.name, "timestamp", timestamp, "n/a"
                 )
 
-                message = " ".join([t.text for t in message_tokens])
-                combine_tokens(*message_tokens).record(
-                    self.name, "message", message, "n/a"
-                )
-
                 contact = datafile.create_contact(
                     data_store=data_store,
                     platform=platform,
+                    sensor=sensor,
                     timestamp=timestamp,
-                    Contact=message,
-                    Contact_type=Contact_type,
                     parser_name=self.short_name,
                 )
-                Contact.privacy = privacy
+                contact.privacy = privacy
+                contact.bearing = bearing
+                contact.location = location
+
+                # sort out the optional fields
+                if range_token.text.upper() != "NULL":
+                    range_val = convert_distance(
+                        range_token.text,
+                        unit_registry.yard,
+                        line,
+                        self.errors,
+                        self.error_type,
+                    )
+                    range_token.record(self.name, "range", range_val, "yds")
+                    # TODO add range field to schema
+                    contact.range = range_val
+
+                if freq_token is not None:
+                    if freq_token.text.upper() != "NULL":
+                        freq = float(freq_token.text)
+                        freq_token.record(self.name, "frequency", freq, "Hz")
+                        contact.freq = freq
+
+                if ambig_bearing_token is not None:
+                    if ambig_bearing_token.text.upper() == "NULL":
+                        bearing = 0
+                    else:
+                        ambig_bearing = convert_absolute_angle(
+                            bearing_token.text, line, self.errors, self.error_type
+                        )
+                        ambig_bearing_token.record(
+                            self.name, "ambig bearing", ambig_bearing, "degs"
+                        )
+                        # TODO - add ambiguous bearing to schema
--- tests/test_load_rep_contact.py
+++ tests/test_load_rep_contact.py
@@ -1,7 +1,8 @@
 import os
 import unittest
+import math
 
-from pepys_import.file.replay_comment_importer import ReplayCommentImporter
+from pepys_import.file.replay_contact_importer import ReplayContactImporter
 from pepys_import.file.file_processor import FileProcessor
 from pepys_import.core.store.data_store import DataStore
 
@@ -12,7 +13,7 @@ DATA_PATH2 = os.path.join(
 )
 
 
-class RepCommentTests(unittest.TestCase):
+class RepContactTests(unittest.TestCase):
     def setUp(self):
         self.store = DataStore("", "", "", 0, ":memory:", db_type="sqlite")
         self.store.initialise()
@@ -20,15 +21,15 @@ class RepCommentTests(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_process_rep_comments(self):
+    def test_process_rep_contacts(self):
         processor = FileProcessor()
-        processor.register_importer(ReplayCommentImporter())
+        processor.register_importer(ReplayContactImporter())
 
         # check states empty
         with self.store.session_scope():
             # there must be no states at the beginning
-            comments = self.store.session.query(self.store.db_classes.Comment).all()
-            self.assertEqual(len(comments), 0)
+            contacts = self.store.session.query(self.store.db_classes.Contact).all()
+            self.assertEqual(len(contacts), 0)
 
             # there must be no platforms at the beginning
             platforms = self.store.session.query(self.store.db_classes.Platform).all()
@@ -44,16 +45,14 @@ class RepCommentTests(unittest.TestCase):
         # check data got created
         with self.store.session_scope():
             # there must be states after the import
-            comments = self.store.session.query(self.store.db_classes.Comment).all()
-            self.assertEqual(len(comments), 7)
-            # check the first two rows have matching comment types (NULL)
-            self.assertEqual(comments[0].comment_type_id, 1)
-            self.assertEqual(comments[1].comment_type_id, 1)
-            # and the last row has a different comment type
-            self.assertEqual(comments[6].comment_type_id, 2)
-            # and the full comments on some other rows
-            self.assertEqual(comments[0].content, "Contact detected on TA")
-            self.assertEqual(comments[6].content, "SUBJECT lost on TA")
+            contacts = self.store.session.query(self.store.db_classes.Contact).all()
+            self.assertEqual(len(contacts), 7)
+
+            # check the data contents
+            self.assertEqual(contacts[0].bearing, math.radians(252.85))
+            self.assertEqual(contacts[1].bearing, math.radians(251.33))
+
+            self.assertEqual(contacts[0].freq, 123.4)
 
             # there must be platforms after the import
             platforms = self.store.session.query(self.store.db_classes.Platform).all()
@@ -63,15 +62,16 @@ class RepCommentTests(unittest.TestCase):
             datafiles = self.store.session.query(self.store.db_classes.Datafile).all()
             self.assertEqual(len(datafiles), 1)
 
-    def test_process_rep_comment_errors(self):
+    @unittest.skip("do successful parse first")
+    def test_process_rep_contact_errors(self):
         processor = FileProcessor()
-        processor.register_importer(ReplayCommentImporter())
+        processor.register_importer(ReplayContactImporter())
 
         # check states empty
         with self.store.session_scope():
             # there must be no states at the beginning
-            comments = self.store.session.query(self.store.db_classes.Comment).all()
-            self.assertEqual(len(comments), 0)
+            contacts = self.store.session.query(self.store.db_classes.Contact).all()
+            self.assertEqual(len(contacts), 0)
 
             # there must be no platforms at the beginning
             platforms = self.store.session.query(self.store.db_classes.Platform).all()
@@ -87,8 +87,8 @@ class RepCommentTests(unittest.TestCase):
         # check data got created
         with self.store.session_scope():
             # there must be states after the import
-            comments = self.store.session.query(self.store.db_classes.Comment).all()
-            self.assertEqual(len(comments), 0)
+            contacts = self.store.session.query(self.store.db_classes.Contact).all()
+            self.assertEqual(len(contacts), 0)
 
             # there must be platforms after the import
             platforms = self.store.session.query(self.store.db_classes.Platform).all()
