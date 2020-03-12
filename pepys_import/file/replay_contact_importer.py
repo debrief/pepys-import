91866f7348fe10522c11adb285ad982db88e50b6
Ian Mayo <ian@planetmayo.com>
2020-03-12 08:29:49 +0000

    test contact importer

---
+++ pepys_import/file/replay_contact_importer.py
@@ -0,0 +1,172 @@
+from .importer import Importer
+from pepys_import.core.validators import constants
+from pepys_import.core.formats.rep_line import parse_timestamp
+from pepys_import.file.highlighter.support.combine import combine_tokens
+from pepys_import.utils.unit_utils import convert_absolute_angle
+from pepys_import.core.formats import unit_registry
+
+
+class ReplayContactImporter(Importer):
+    def __init__(
+        self,
+        name="Replay Contact Importer",
+        validation_level=constants.ENHANCED_LEVEL,
+        short_name="REP Contact Importer",
+    ):
+        super().__init__(name, validation_level, short_name)
+        self.text_label = None
+        self.depth = 0.0
+        self.errors = list()
+
+    def can_load_this_type(self, suffix):
+        return suffix.upper() == ".REP"
+
+    def can_load_this_filename(self, filename):
+        return True
+
+    def can_load_this_header(self, first_line):
+        return True
+
+    def can_load_this_file(self, file_contents):
+        return True
+
+    def _load_this_file(self, data_store, path, file_object, datafile):
+        for line_number, line in enumerate(file_object.lines(), 1):
+            if line.text.startswith(";"):
+                if line.text.startswith(";SENSOR:"):
+                    # ok for for it
+                    tokens = line.tokens()
+
+                    if len(tokens) < 5:
+                        self.errors.append(
+                            {
+                                self.error_type: f"Error on line {line_number}. "
+                                f"Not enough tokens: {line.text}"
+                            }
+                        )
+                        continue
+
+                    # separate token strings
+                    date_token = tokens[1]
+                    time_token = tokens[2]
+                    vessel_name_token = tokens[3]
+                    symbology = tokens[4]
+
+                    # the next one may be degs, or it may be NULL
+                    next_token = tokens[5]
+                    if next_token.text.upper() == "NULL":
+                        location = None
+                        token_ctr = 6
+                    else:
+                        token_ctr = 13
+                        lat_degrees_token = tokens[5]
+                        lat_mins_token = tokens[6]
+                        lat_secs_token = tokens[7]
+                        lat_hemi_token = tokens[8]
+                        long_degrees_token = tokens[9]
+                        long_mins_token = tokens[10]
+                        long_secs_token = tokens[11]
+                        long_hemi_token = tokens[12]
+
+                        tatitude = Location(
+                            lat_degrees_token.text,
+                            lat_mins_token.text,
+                            lat_secs_token.text,
+                            lat_hemi_token.text,
+                            errors,
+                            error_type,
+                        )
+                        combine_tokens(
+                            lat_degrees_token,
+                            lat_mins_token,
+                            lat_secs_token,
+                            lat_hemi_token,
+                        ).record(self.importer_name, "latitude", self.latitude, "DMS")
+
+                        longitude = Location(
+                            long_degrees_token.text,
+                            long_mins_token.text,
+                            long_secs_token.text,
+                            long_hemi_token.text,
+                            errors,
+                            error_type,
+                        )
+                        combine_tokens(
+                            long_degrees_token,
+                            long_mins_token,
+                            long_secs_token,
+                            long_hemi_token,
+                        ).record(self.importer_name, "longitude", self.longitude, "DMS")
+
+                        location = (
+                            f"POINT({self.longitude.as_degrees()} "
+                            f"{self.latitude.as_degrees()})"
+                        )
+
+                    token_ctr += 1
+                    bearing_token = tokens[token_ctr]
+                    if bearing_token.text.upper() == "NULL":
+                        bearing = 0
+                    else:
+                        bearing = convert_absolute_angle(
+                            bearing_token.text, line, self.errors, self.error_type
+                        )
+
+                    token_ctr += 1
+                    range_token = tokens[token_ctr]
+                    if range_token.text.upper() == "NULL":
+                        range = 0
+                    else:
+                        range = convert_distance(
+                            range_token.text,
+                            unit_registry.yard,
+                            line,
+                            self.errors,
+                            self.error_type,
+                        )
+
+                    token_ctr += 1
+                    sensor_name = tokens[token_ctr]
+                    token_ctr += 1
+                    label = tokens[token_ctr:]
+                else:
+                    continue
+
+                privacy = data_store.missing_data_resolver.resolve_privacy(data_store)
+                platform = data_store.get_platform(
+                    platform_name=vessel_name_token.text,
+                    nationality="UK",
+                    platform_type="Fisher",
+                    privacy="Public",
+                )
+                vessel_name_token.record(
+                    self.name, "vessel name", vessel_name_token.text, "n/a"
+                )
+                sensor_type = data_store.add_to_sensor_types("Human")
+                sensor = platform.get_sensor(
+                    data_store=data_store,
+                    sensor_name=platform.name,
+                    sensor_type=sensor_type,
+                    privacy=privacy.name,
+                )
+                comment_type = data_store.add_to_comment_types(comment_type)
+
+                timestamp = parse_timestamp(date_token.text, time_token.text)
+                combine_tokens(date_token, time_token).record(
+                    self.name, "timestamp", timestamp, "n/a"
+                )
+
+                message = " ".join([t.text for t in message_tokens])
+                combine_tokens(*message_tokens).record(
+                    self.name, "message", message, "n/a"
+                )
+
+                contact = datafile.create_contact(
+                    data_store=data_store,
+                    platform=platform,
+                    timestamp=timestamp,
+                    Contact=message,
+                    Contact_type=Contact_type,
+                    parser_name=self.short_name,
+                )
+                Contact.privacy = privacy
