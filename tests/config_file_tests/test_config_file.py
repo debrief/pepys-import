b6d54f8f5e81818714b5a53d29c7e429da3d4599
Baris Sari <sariba@itu.edu.tr>
2020-03-13 15:01:40 +0300

    Correct environment variable tests

--- tests/config_file_tests/test_config_file.py
+++ tests/config_file_tests/test_config_file.py
@@ -8,27 +8,21 @@ from unittest.mock import patch
 from pepys_import.file.file_processor import FileProcessor
 
 DIRECTORY_PATH = os.path.dirname(__file__)
-TEST_IMPORTER_PATH = os.path.join(DIRECTORY_PATH, "test_local_imports")
+TEST_IMPORTER_PATH = os.path.join(DIRECTORY_PATH, "samples")
 BAD_IMPORTER_PATH = os.path.join(DIRECTORY_PATH, "bad_path")
+OUTPUT_PATH = os.path.join(DIRECTORY_PATH, "output")
 
 
-# TODO: Convert these tests into config parsing tests
-"""
 class EnvironmentVariablesTestCase(unittest.TestCase):
-    @patch("config.LOCAL_PARSERS", TEST_IMPORTER_PATH)
+    @patch("pepys_import.file.file_processor.LOCAL_PARSERS", TEST_IMPORTER_PATH)
     def test_pepys_local_parsers(self):
-        from config import LOCAL_PARSERS
-        assert LOCAL_PARSERS == TEST_IMPORTER_PATH
-
         file_processor = FileProcessor()
+
         assert len(file_processor.importers) == 1
         assert file_processor.importers[0].name == "Test Importer"
 
-    @patch("config.LOCAL_PARSERS", BAD_IMPORTER_PATH)
+    @patch("pepys_import.file.file_processor.LOCAL_PARSERS", BAD_IMPORTER_PATH)
     def test_bad_pepys_local_parsers_path(self):
-        from config import LOCAL_PARSERS
-        assert LOCAL_PARSERS == BAD_IMPORTER_PATH
-
         temp_output = StringIO()
         with redirect_stdout(temp_output):
             file_processor = FileProcessor()
@@ -37,21 +31,17 @@ class EnvironmentVariablesTestCase(unittest.TestCase):
         assert len(file_processor.importers) == 0
         assert (
             output
-            == f"No such file or directory: {LOCAL_PARSERS}. Only core parsers are going to work.\n"
+            == f"No such file or directory: {BAD_IMPORTER_PATH}. Only core parsers are going to work.\n"
         )
 
-    @patch.dict(os.environ, {"PEPYS_ARCHIVE_LOCATION": "./test_output/"})
+    @patch("pepys_import.file.file_processor.ARCHIVE_PATH", OUTPUT_PATH)
     def test_pepys_archive_location(self):
-        value = os.getenv("PEPYS_ARCHIVE_LOCATION")
-        assert value == "./test_output/"
-
         file_processor = FileProcessor()
-        assert os.path.exists(value) is True
-        assert file_processor.output_path == value
+        assert os.path.exists(OUTPUT_PATH) is True
+        assert file_processor.output_path == OUTPUT_PATH
         # Remove the test_output directory
-        os.rmdir(value)
+        os.rmdir(OUTPUT_PATH)
 
 
 if __name__ == "__main__":
     unittest.main()
-"""
