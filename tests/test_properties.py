e107d244a1adf0b2106fd3b4e7ab1a47537ce142
Robin Wilson <robin@rtwilson.com>
2020-03-15 19:36:16 +0000

    Add tests for new heading property

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -98,3 +98,50 @@ class TestStateElevationProperty(unittest.TestCase):
 
         assert state.elevation == 10 * unit_registry.metre
         assert state.elevation.check("[length]")
+
+
+class TestStateHeadingProperty(unittest.TestCase):
+    def setUp(self):
+        self.store = DataStore("", "", "", 0, ":memory:", db_type="sqlite")
+        self.store.initialise()
+
+    def tearDown(self):
+        pass
+
+    def test_state_heading_scalar(self):
+        state = self.store.db_classes.State()
+
+        # Check setting with a scalar (float) gives error
+        with pytest.raises(TypeError) as exception:
+            state.heading = 5
+
+        assert "Heading must be a Quantity" in str(exception.value)
+
+    def test_state_heading_wrong_units(self):
+        state = self.store.db_classes.State()
+
+        # Check setting with a Quantity of the wrong units gives error
+        with pytest.raises(ValueError) as exception:
+            state.heading = 5 * unit_registry.second
+
+        assert "Heading must be a Quantity with a dimensionality of ''" in str(
+            exception.value
+        )
+
+    def test_state_heading_right_units(self):
+        state = self.store.db_classes.State()
+
+        # Check setting with a Quantity of the right SI units succeeds
+        state.heading = 57 * unit_registry.degree
+
+        # Check setting with a Quantity of strange but valid units succeeds
+        state.heading = 0.784 * unit_registry.radian
+
+    def test_state_heading_roundtrip(self):
+        state = self.store.db_classes.State()
+
+        # Check setting and retrieving field works, and gives units as a result
+        state.heading = 157 * unit_registry.degree
+
+        assert state.heading == 157 * unit_registry.degree
+        assert state.heading.check("")
