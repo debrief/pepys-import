165cee0ee2c8cc6b04efcfe28c74853c9898d049
Baris Sari <sariba@itu.edu.tr>
2020-03-21 16:11:11 +0300

    Remove db argument and unnecessary blank lines

--- pepys_import/import.py
+++ pepys_import/import.py
@@ -14,9 +14,7 @@ def main(path=DIRECTORY_PATH, db=DEFAULT_DATABASE, archive=False):
     data_store.initialise()
 
     processor = FileProcessor(archive=archive)
-
     processor.load_importers_dynamically()
-
     processor.process(path, data_store, True)
 
 
@@ -24,14 +22,12 @@ if __name__ == "__main__":
     # Parse arguments
     parser = argparse.ArgumentParser()
     path_help = "The path to import data from (The default value is the directory of the script)"
-    db_help = "The database to connect to: (The is to use an in-memory database)"
     archive_help = (
         " Instruction to archive (move) imported files to designated archive folder"
     )
     parser.add_argument(
         "--path", help=path_help, required=False, default=DIRECTORY_PATH
     )
-    parser.add_argument("--db", help=db_help, required=False, default=DEFAULT_DATABASE)
     parser.add_argument(
         "--archive",
         dest="archive",
