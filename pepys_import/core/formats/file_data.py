89b3060939eb1e90a2d72f12d32d29a9e4496ceb
Baris Sari <sariba@itu.edu.tr>
2020-03-25 13:01:37 +0300

    Remove unnecessary file

--- pepys_import/core/formats/file_data.py
+++
@@ -1,3 +0,0 @@
-# TODO: these will be the types that all file importers will create
-#       the DataStore methods will work on these types, agnostic of the initial
-#       file type they came from
