72bb8fe8a322604c055f06d29386c6f6daf4286c
Baris Sari <sariba@itu.edu.tr>
2020-03-26 14:44:28 +0300

    Use rep_test1.rep for export datafile tests

--- tests/test_export_data.py
+++ tests/test_export_data.py
@@ -1,13 +1,20 @@
+import os
 import unittest
 from unittest import TestCase
 
 from testing.postgresql import Postgresql
 
 from pepys_import.core.store.data_store import DataStore
+from pepys_import.file.file_processor import FileProcessor
+
+FILE_PATH = os.path.dirname(__file__)
+CURRENT_DIR = os.getcwd()
+DATA_PATH = os.path.join(FILE_PATH, "sample_data/track_files/rep_data/rep_test1.rep")
 
 
 class DataStoreExportPostGISDBTestCase(TestCase):
     def setUp(self):
+        self.path = os.path.join(CURRENT_DIR, "export_test.rep")
         self.store = None
         try:
             self.store = Postgresql(
@@ -16,18 +23,19 @@ class DataStoreExportPostGISDBTestCase(TestCase):
         except RuntimeError:
             print("PostgreSQL database couldn't be created! Test is skipping.")
 
-    def tearDown(self):
+    def tearDown(self) -> None:
+        if os.path.exists(self.path):
+            os.remove(self.path)
         try:
             self.store.stop()
         except AttributeError:
             return
 
-    def test_postgres_cleardb(self):
-        """Test whether all database tables are empty"""
+    def test_postgres_clear_db(self):
         if self.store is None:
             self.skipTest("Postgres is not available. Test is skipping")
 
-        data_store_postgres = DataStore(
+        data_store = DataStore(
             db_username="postgres",
             db_password="postgres",
             db_host="localhost",
@@ -35,53 +43,89 @@ class DataStoreExportPostGISDBTestCase(TestCase):
             db_name="test",
             db_type="postgres",
         )
+        data_store.initialise()
 
-        # creating database from schema
-        data_store_postgres.initialise()
+        # Parse the file
+        processor = FileProcessor(archive=False)
+        processor.load_importers_dynamically()
+        processor.process(DATA_PATH, data_store, False)
 
-        with data_store_postgres.session_scope():
-            # populate data
-            data_store_postgres.populate_reference()
-            data_store_postgres.populate_metadata()
-            data_store_postgres.populate_measurement()
+        with data_store.session_scope():
+            datafiles = data_store.get_all_datafiles()
+            selected_datafile_id = datafiles[0].datafile_id
+            data_store.export_datafile(selected_datafile_id, self.path)
 
-            datafiles = data_store_postgres.get_all_datafiles()
-            datafiles_dict = {}
-            for datafile in datafiles:
-                datafiles_dict[datafile.reference] = datafile.datafile_id
-        datafile_reference = "DATAFILE-1"
-        selected_datafile_id = datafiles_dict[datafile_reference]
-        with data_store_postgres.session_scope():
-            data_store_postgres.export_datafile(selected_datafile_id, datafile_reference)
+            # Fetch data from the exported file
+            with open(self.path, "r") as file:
+                data = file.read().split("\n")
 
-        # self.assertNotEqual(len(records), 0)
+            assert (
+                "100112 115800.000 SUBJECT AA 60 23 40.25 N 000 01 25.86 E 109.08 6.00 0.0" in data
+            )
+            assert (
+                "100112 121400.000 SEARCH_PLATFORM AA 60 28 8.02 N 000 35 59.95 E 179.84 8.00 0.0"
+                in data
+            )
+            assert ";NARRATIVE: 100112 115800.000 SENSOR Contact detected on TA" in data
+            assert (
+                ";NARRATIVE2: 100112 121200.000 SEARCH_PLATFORM OBSERVATION SUBJECT lost on TA"
+                in data
+            )
+            assert (
+                ";SENSOR2: 100112 115800.000 SENSOR @@ NULL 252.85 NULL 123.4 hertz 432.10 SENSOR N/A"
+                in data
+            )
+            assert (
+                ";SENSOR: 100112 120000.000 SENSOR @@ 60 15 00 N 016 45 00 E 251.33 NULL SENSOR N/A"
+                in data
+            )
 
 
 class DataStoreExportSpatiaLiteTestCase(TestCase):
-    def test_sqlite_cleardb(self):
-        """Test whether all database tables are empty"""
-        data_store_sqlite = DataStore("", "", "", 0, ":memory:", db_type="sqlite")
+    def setUp(self) -> None:
+        self.path = os.path.join(CURRENT_DIR, "export_test.rep")
+        self.store = DataStore("", "", "", 0, ":memory:", db_type="sqlite")
+        self.store.initialise()
 
-        # creating database from schema
-        data_store_sqlite.initialise()
+        # Parse the file
+        processor = FileProcessor(archive=False)
+        processor.load_importers_dynamically()
+        processor.process(DATA_PATH, self.store, False)
 
-        with data_store_sqlite.session_scope():
-            # populate data
-            data_store_sqlite.populate_reference()
-            data_store_sqlite.populate_metadata()
-            data_store_sqlite.populate_measurement()
+    def tearDown(self) -> None:
+        if os.path.exists(self.path):
+            os.remove(self.path)
 
-        with data_store_sqlite.session_scope():
-            datafiles = data_store_sqlite.get_all_datafiles()
-            datafiles_dict = {}
-            for datafile in datafiles:
-                datafiles_dict[datafile.reference] = datafile.datafile_id
-        datafile_reference = "DATAFILE-1"
-        selected_datafile_id = datafiles_dict[datafile_reference]
-        with data_store_sqlite.session_scope():
-            data_store_sqlite.export_datafile(selected_datafile_id, datafile_reference)
+    def test_sqlite_clear_db(self):
+        with self.store.session_scope():
+            datafiles = self.store.get_all_datafiles()
+            selected_datafile_id = datafiles[0].datafile_id
+            self.store.export_datafile(selected_datafile_id, self.path)
 
-        # self.assertNotEqual(len(records), 0)
+            # Fetch data from the exported file
+            with open(self.path, "r") as file:
+                data = file.read().split("\n")
+
+            assert (
+                "100112 115800.000 SUBJECT AA 60 23 40.25 N 000 01 25.86 E 109.08 6.00 0.0" in data
+            )
+            assert (
+                "100112 121400.000 SEARCH_PLATFORM AA 60 28 8.02 N 000 35 59.95 E 179.84 8.00 0.0"
+                in data
+            )
+            assert ";NARRATIVE: 100112 115800.000 SENSOR Contact detected on TA" in data
+            assert (
+                ";NARRATIVE2: 100112 121200.000 SEARCH_PLATFORM OBSERVATION SUBJECT lost on TA"
+                in data
+            )
+            assert (
+                ";SENSOR2: 100112 115800.000 SENSOR @@ NULL 252.85 NULL 123.4 hertz 432.10 SENSOR N/A"
+                in data
+            )
+            assert (
+                ";SENSOR: 100112 120000.000 SENSOR @@ 60 15 00 N 016 45 00 E 251.33 NULL SENSOR N/A"
+                in data
+            )
 
 
 if __name__ == "__main__":
