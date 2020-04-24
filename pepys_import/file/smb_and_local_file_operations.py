6217815b0457e5b4830781bfa0eb830f88b8f794
Robin Wilson <robin@rtwilson.com>
2020-04-24 15:07:32 +0100

    Removed duplication of try-except blocks, replacing with a contextmanager function

--- pepys_import/file/smb_and_local_file_operations.py
+++ pepys_import/file/smb_and_local_file_operations.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import sys
+from contextlib import contextmanager
 from stat import S_IREAD
 
 import smbclient
@@ -10,10 +11,18 @@ from smbprotocol.exceptions import SMBAuthenticationError, SMBResponseException
 
 from config import ARCHIVE_ON_SMB, ARCHIVE_PASSWORD, ARCHIVE_USER
 
-SMB_ERROR_MESSAGE = (
-    "Error connecting to archive location on Windows shared folder (SMB share). "
-    + "Check config file details are correct and server is accessible. See full error above."
-)
+
+@contextmanager
+def handle_smb_errors():
+    try:
+        yield
+    except (SMBAuthenticationError, SMBResponseException, ValueError) as e:
+        print(e)
+        print(
+            "Error connecting to archive location on Windows shared folder (SMB share). "
+            + "Check config file details are correct and server is accessible. See full error above."
+        )
+        sys.exit()
 
 
 auth = {"username": ARCHIVE_USER, "password": ARCHIVE_PASSWORD}
@@ -21,75 +30,51 @@ auth = {"username": ARCHIVE_USER, "password": ARCHIVE_PASSWORD}
 
 def exists(path):
     if ARCHIVE_ON_SMB:
-        try:
-            return smbclient.path.exists(path, **auth)
-        except (SMBAuthenticationError, SMBResponseException, ValueError) as e:
-            print(e)
-            print(SMB_ERROR_MESSAGE)
-            sys.exit()
+        with handle_smb_errors():
+            smbclient.path.exists(path, **auth)
     else:
         return os.path.exists(path)
 
 
 def isdir(path):
     if ARCHIVE_ON_SMB:
-        try:
+        with handle_smb_errors():
             return smbclient.path.isdir(path, **auth)
-        except (SMBAuthenticationError, SMBResponseException, ValueError) as e:
-            print(e)
-            print(SMB_ERROR_MESSAGE)
-            sys.exit()
     else:
         return os.path.isdir(path)
 
 
 def makedirs(path):
     if ARCHIVE_ON_SMB:
-        try:
+        with handle_smb_errors():
             return smbclient.makedirs(path, **auth)
-        except (SMBAuthenticationError, SMBResponseException, ValueError) as e:
-            print(e)
-            print(SMB_ERROR_MESSAGE)
-            sys.exit()
     else:
         return os.makedirs(path)
 
 
 def move(from_path, to_path):
     if ARCHIVE_ON_SMB:
-        try:
+        with handle_smb_errors():
             # No move function in smbclient, so copy then delete original copy
             smbclient.shutil.copy(from_path, to_path, **auth)
             os.remove(from_path)
-        except (SMBAuthenticationError, SMBResponseException, ValueError) as e:
-            print(e)
-            print(SMB_ERROR_MESSAGE)
-            sys.exit()
     else:
         shutil.move(from_path, to_path)
 
 
 def set_read_only(path):
     if ARCHIVE_ON_SMB:
-        try:
+        with handle_smb_errors():
             smbclient.shutil._set_file_basic_info(
                 path, follow_symlinks=False, read_only=True, **auth
             )
-        except (SMBAuthenticationError, SMBResponseException, ValueError) as e:
-            print(e)
-            print(SMB_ERROR_MESSAGE)
-            sys.exit()
     else:
         os.chmod(path, S_IREAD)
 
 
 def open_file(*args, **kwargs):
     if ARCHIVE_ON_SMB:
-        try:
+        with handle_smb_errors():
             return smbclient.open_file(*args, **kwargs, **auth)
-        except (SMBAuthenticationError, SMBResponseException, ValueError) as e:
-            print(e)
-            print(SMB_ERROR_MESSAGE)
-            sys.exit()
     else:
         return open(*args, **kwargs)
