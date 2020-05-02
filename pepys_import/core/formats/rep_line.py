ca4b0d1ed9da4d79e30514342843e3405ced540c
Robin Wilson <robin@rtwilson.com>
2020-05-02 19:05:40 +0100

    Minor fix to REP importer to move unit setting for depth to REPLine class, like all other variables
    This means it comes up with the correct units in the highlighted output

--- pepys_import/core/formats/rep_line.py
+++ pepys_import/core/formats/rep_line.py
@@ -183,7 +183,7 @@ class REPLine:
         speed_token.record(self.importer_name, "speed", self.speed)
 
         try:
-            self.depth = float(depth_token.text)
+            self.depth = float(depth_token.text) * unit_registry.metre
             if isnan(self.depth):
                 self.depth = None
         except ValueError:
