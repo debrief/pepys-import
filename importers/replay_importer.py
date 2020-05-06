a989859ba005e4bcdf3a6522d6b0a6e60b246310
Robin Wilson <robin@rtwilson.com>
2020-05-06 16:49:51 +0100

    Don't hardcode sensor name as platform name in REP importer

--- importers/replay_importer.py
+++ importers/replay_importer.py
@@ -44,9 +44,7 @@ class ReplayImporter(Importer):
         vessel_name = rep_line.get_platform()
         platform = data_store.get_platform(platform_name=vessel_name, change_id=change_id,)
 
-        sensor = platform.get_sensor(
-            data_store=data_store, sensor_name=platform.name, change_id=change_id,
-        )
+        sensor = platform.get_sensor(data_store=data_store, sensor_name=None, change_id=change_id,)
         state = datafile.create_state(
             data_store, platform, sensor, rep_line.timestamp, self.short_name,
         )
