389f4f58653643feb4a175ab979690c3f184ba8c
Ian Mayo <ian@planetmayo.com>
2020-05-28 10:15:53 +0100

    fix other name mangling

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/debrief/pepys-import",
-    version="version='0.0.12'",
+    version="0.0.12",
     zip_safe=False,
 )
