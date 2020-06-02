33dd14ba9422282eef249fd061f9fd167cd1b824
Ian Mayo <ian@planetmayo.com>
2020-06-02 12:45:08 +0100

    Switch to verbose output

--- pepys_admin/admin_cli.py
+++ pepys_admin/admin_cli.py
@@ -82,7 +82,7 @@ class AdminShell(cmd.Cmd):
             print(f"## Reference\n{report}\n")
 
         print(f"## Database Version")
-        command.current(self.cfg)
+        command.current(self.cfg, verbose=True)
 
     def do_migrate(self):
         print("Alembic migration command running, see output below.")
