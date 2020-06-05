05bf8d15d4463d4c6908e4de8d030fcb8ee34653
Robin Wilson <robin@rtwilson.com>
2020-06-05 15:16:30 +0100

    Change to use constants table

--- pepys_import/core/store/data_store.py
+++ pepys_import/core/store/data_store.py
@@ -401,7 +401,7 @@ class DataStore:
 
     def add_to_synonyms(self, table, name, entity, change_id):
         # Blacklist certain tables, and don't Synonyms for them be created
-        if table in ["Sensors", "GeometrySubTypes"]:
+        if table in [constants.SENSOR, constants.GEOMETRY_SUBTYPE]:
             raise Exception(f"Synonyms are not allowed for table {table}")
 
         # enough info to proceed and create entry
