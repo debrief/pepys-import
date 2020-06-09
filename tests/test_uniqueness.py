4ba62b9b9e8b42e56669049ab1c2213dc1630581
Robin Wilson <robin@rtwilson.com>
2020-06-09 13:22:17 +0100

    Update to pass level to Privacy

--- tests/test_uniqueness.py
+++ tests/test_uniqueness.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm.session import sessionmaker
 from pepys_import.core.store import constants
 from pepys_import.core.store.data_store import DataStore
 
-# All reference tables excluding GeometrySubType which can't be tested in this simple way
+# All reference tables excluding GeometrySubType and Privacy which can't be tested in this simple way
 REFERENCE_TABLES = [
     "SensorType",
     "PlatformType",
@@ -13,7 +13,6 @@ REFERENCE_TABLES = [
     "GeometryType",
     "User",
     "UnitType",
-    "Privacy",
     "DatafileType",
     "MediaType",
     "CommentType",
@@ -37,13 +36,28 @@ class TestUniqueness:
         db_session = sessionmaker(bind=self.store.engine)
         session = db_session()
 
-        # Add sensor type Blah
+        # Add object of given type
         table = getattr(self.store.db_classes, table_name)
-        sensor_type = table(name="Blah")
-        session.add(sensor_type)
+        obj = table(name="Blah")
+        session.add(obj)
         session.flush()
 
         with pytest.raises(exc.IntegrityError):
-            sensor_type = table(name="Blah")
-            session.add(sensor_type)
+            obj = table(name="Blah")
+            session.add(obj)
+            session.flush()
+
+    def test_privacy_uniqueness(self):
+        db_session = sessionmaker(bind=self.store.engine)
+        session = db_session()
+
+        # Add privacy
+        obj = self.store.db_classes.Privacy(name="Blah", level=0)
+        session.add(obj)
+        session.flush()
+
+        # Try adding with same name but different level - should still fail
+        with pytest.raises(exc.IntegrityError):
+            obj = self.store.db_classes.Privacy(name="Blah", level=20)
+            session.add(obj)
             session.flush()
