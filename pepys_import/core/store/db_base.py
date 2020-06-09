bdc25590e71501587d655c69eb43b3c2d6368ffc
Baris Sari <sariba@itu.edu.tr>
2020-06-09 20:56:08 +0300

    Declare dictionaries outside of metadata

--- pepys_import/core/store/db_base.py
+++ pepys_import/core/store/db_base.py
@@ -5,24 +5,22 @@ from sqlalchemy.ext.declarative import declarative_base
 # constraints create problems for migration. Therefore, naming_convention is passed to declarative
 # base of SQLite:
 # https://docs.sqlalchemy.org/en/13/core/constraints.html#configuring-constraint-naming-conventions
-meta = MetaData(
-    naming_convention={
-        "ix": "ix_%(column_0_label)s",
-        "uq": "uq_%(table_name)s_%(column_0_name)s",
-        "ck": "ck_%(table_name)s_%(column_0_name)s",
-        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-        "pk": "pk_%(table_name)s",
-    }
-)
-meta_postgres = MetaData(
-    naming_convention={
-        "ix": "ix_%(column_0_N_label)s",
-        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
-        "ck": "ck_%(table_name)s_%(constraint_name)s",
-        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-        "pk": "pk_%(table_name)s",
-    }
-)
+sqlite_naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+postgres_naming_convention = {
+    "ix": "ix_%(column_0_N_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+meta_sqlite = MetaData(naming_convention=sqlite_naming_convention)
+meta_postgres = MetaData(naming_convention=postgres_naming_convention)
 # define this as the base for all the DB tables here in a common module
+BaseSpatiaLite = declarative_base(metadata=meta_sqlite)
 BasePostGIS = declarative_base(metadata=meta_postgres)
-BaseSpatiaLite = declarative_base(metadata=meta)
