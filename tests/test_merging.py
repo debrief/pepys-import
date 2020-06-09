0f7a988c46ef8214287197a95566c12c840f7f2d
Robin Wilson <robin@rtwilson.com>
2020-06-09 14:30:09 +0100

    Added export-alter-merge test for Postgres too

--- tests/test_merging.py
+++ tests/test_merging.py
@@ -1971,8 +1971,272 @@ class TestExportAlterAndMerge(unittest.TestCase):
         # We import these files:  uk_track.rep
         new_default_resolver = DefaultResolver()
         new_default_resolver.default_platform_name = "PLATFORM-1-SLAVE"
-        new_default_resolver.default_trigraph = "PL1-SLAVE"
-        new_default_resolver.default_quadgraph = "PLT1-SLAVE"
+        new_default_resolver.default_trigraph = "SL1"
+        new_default_resolver.default_quadgraph = "SLT1"
+        new_default_resolver.default_identifier = "123-SLAVE"
+        new_default_resolver.default_platform_type = "Warship-SLAVE"
+        new_default_resolver.default_nationality = "UK-SLAVE"
+        new_default_resolver.default_sensor_name = "SENSOR-1-SLAVE"
+        new_default_resolver.default_sensor_type = "Position-SLAVE"
+        new_default_resolver.default_privacy = "PRIVACY-1-SLAVE"
+        new_default_resolver.default_datafile_name = "DATAFILE-1-SLAVE"
+        new_default_resolver.default_datafile_type = "DATAFILE-TYPE-1-SLAVE"
+
+        self.slave_store = DataStore(
+            "",
+            "",
+            "",
+            0,
+            db_name="slave_exported.sqlite",
+            db_type="sqlite",
+            missing_data_resolver=new_default_resolver,
+        )
+
+        processor.process(
+            os.path.join(SAMPLE_DATA_PATH, "track_files", "rep_data", "uk_track.rep"),
+            self.slave_store,
+            False,
+        )
+
+        # Do some manual creation of objects in master
+        # Specifically:
+        #  - a master-specific PlatformType, and a shared-by-name PlatformType
+        #  - a master-specific Platform and a shared-by-name Platform
+        #  - a master-specific and shared-by-name Synonym
+
+        # Create a change ID first
+        with self.master_store.session_scope():
+            change_id = self.master_store.add_to_changes(
+                "TEST", datetime.utcnow(), "TEST"
+            ).change_id
+            self.master_store.add_to_platform_types("Master_PT_1", change_id=change_id)
+            self.master_store.add_to_platform_types("Shared_PT_1", change_id=change_id)
+
+            m_plat_1 = self.master_store.add_to_platforms(
+                "Master_Platform_1", "123", "UK", "Warship", "Public", change_id=change_id
+            )
+            shared_plat_1 = self.master_store.add_to_platforms(
+                "Shared_Platform_1", "234", "UK", "Warship", "Public", change_id=change_id
+            )
+
+            self.master_store.add_to_synonyms(
+                "Platforms", "Master_Platform_1_Synonym", m_plat_1.platform_id, change_id=change_id
+            )
+            self.master_store.add_to_synonyms(
+                "Platforms",
+                "Shared_Platform_1_Synonym",
+                shared_plat_1.platform_id,
+                change_id=change_id,
+            )
+
+        # Do some manual creation of objects in slave
+        # Specifically
+        #  - a slave-specific PlatformType, and a shared-by-name PlatformType (from above)
+        #  - a slave-specific Platform and a shared-by-name Platform (from above)
+        #  - a slave-specific Synonym and a shared-by-name Synonym (from above)
+
+        # Create a change ID first
+        with self.slave_store.session_scope():
+            change_id = self.slave_store.add_to_changes("TEST", datetime.utcnow(), "TEST").change_id
+
+            self.slave_store.add_to_platform_types("Slave_PT_1", change_id=change_id)
+            self.slave_store.add_to_platform_types("Shared_PT_1", change_id=change_id)
+
+            s_plat_1 = self.slave_store.add_to_platforms(
+                "Slave_Platform_1", "123", "UK", "Warship", "Public", change_id=change_id
+            )
+            shared_plat_1 = self.slave_store.add_to_platforms(
+                "Shared_Platform_1", "234", "UK", "Warship", "Public", change_id=change_id
+            )
+
+            self.slave_store.add_to_synonyms(
+                "Platforms", "Slave_Platform_1_Synonym", s_plat_1.platform_id, change_id=change_id
+            )
+            self.slave_store.add_to_synonyms(
+                "Platforms",
+                "Shared_Platform_1_Synonym",
+                shared_plat_1.platform_id,
+                change_id=change_id,
+            )
+
+    def tearDown(self):
+        # if os.path.exists("master.sqlite"):
+        #     os.remove("master.sqlite")
+
+        # if os.path.exists("slave.sqlite"):
+        #     os.remove("slave.sqlite")
+        pass
+
+    def test_export_alter_merge(self):
+        # Do the merge
+        merge_all_tables(self.master_store, self.slave_store)
+
+        with self.master_store.session_scope():
+            with self.slave_store.session_scope():
+                # Check we have four datafiles in master
+                datafiles = self.master_store.session.query(
+                    self.master_store.db_classes.Datafile
+                ).all()
+
+                assert len(datafiles) == 4
+
+                # Check one of these datafiles is called uk_track.rep
+                references = [d.reference for d in datafiles]
+
+                assert "uk_track.rep" in references
+
+                # Check we have 425 rows in States
+                states_count = self.master_store.session.query(
+                    self.master_store.db_classes.State
+                ).count()
+
+                assert states_count == 425
+
+                # Check we have objects with the slave's new default resolver values
+
+                # Platform
+                platform_results = (
+                    self.master_store.session.query(self.master_store.db_classes.Platform)
+                    .filter(self.master_store.db_classes.Platform.identifier == "123-SLAVE")
+                    .all()
+                )
+
+                assert len(platform_results) == 1
+
+                # Privacy
+                privacy_results = (
+                    self.master_store.session.query(self.master_store.db_classes.Privacy)
+                    .filter(self.master_store.db_classes.Privacy.name == "PRIVACY-1-SLAVE")
+                    .all()
+                )
+
+                assert len(privacy_results) == 1
+
+                # Check we have merged PlatformTypes correctly
+                pt_results = self.master_store.session.query(
+                    self.master_store.db_classes.PlatformType
+                ).all()
+
+                pt_names = [pt.name for pt in pt_results]
+
+                assert len(pt_names) == 5
+
+                assert "Master_PT_1" in pt_names
+                assert "Shared_PT_1" in pt_names
+                assert "Slave_PT_1" in pt_names
+
+                # Check we have merged Platforms correctly
+                platform_results = self.master_store.session.query(
+                    self.master_store.db_classes.Platform
+                ).all()
+
+                platform_names = [p.name for p in platform_results]
+
+                assert len(platform_names) == 8
+
+                assert "Master_Platform_1" in platform_names
+                assert "Slave_Platform_1" in platform_names
+                assert "Shared_Platform_1" in platform_names
+
+                # Check we have merged Synonyms properly
+                synonym_results = self.master_store.session.query(
+                    self.master_store.db_classes.Synonym
+                ).all()
+
+                assert len(synonym_results) == 3
+
+                # All Synonyms should link to an entry in the Platforms table
+                for synonym_result in synonym_results:
+                    platforms = (
+                        self.master_store.session.query(self.master_store.db_classes.Platform)
+                        .filter(
+                            self.master_store.db_classes.Platform.platform_id
+                            == synonym_result.entity
+                        )
+                        .all()
+                    )
+
+                    assert len(platforms) == 1
+
+
+@pytest.mark.postgres
+class TestExportAlterAndMerge_Postgres(unittest.TestCase):
+    @patch("pepys_admin.snapshot_cli.input", return_value="slave_exported.sqlite")
+    @patch("pepys_admin.snapshot_cli.iterfzf", return_value=["Public"])
+    def setUp(self, patched_input, patched_iterfzf):
+        if os.path.exists("slave_exported.sqlite"):
+            os.remove("slave_exported.sqlite")
+
+        self.postgres = None
+
+        try:
+            self.postgres = Postgresql(
+                database="test", host="localhost", user="postgres", password="postgres", port=55527,
+            )
+        except RuntimeError:
+            print("PostgreSQL database couldn't be created! Test is skipping.")
+            return
+
+        self.master_store = DataStore(
+            db_name="test",
+            db_host="localhost",
+            db_username="postgres",
+            db_password="postgres",
+            db_port=55527,
+            db_type="postgres",
+        )
+
+        # Init master store
+        self.master_store.initialise()
+
+        # Do some imports into master
+        # We import these files: gpx_1_0.gpx, rep_test1.rep
+        processor = FileProcessor(archive=False)
+        processor.load_importers_dynamically()
+        processor.process(
+            os.path.join(SAMPLE_DATA_PATH, "track_files", "gpx", "gpx_1_0.gpx"),
+            self.master_store,
+            False,
+        )
+        processor.process(
+            os.path.join(SAMPLE_DATA_PATH, "track_files", "rep_data", "rep_test1.rep"),
+            self.master_store,
+            False,
+        )
+
+        # Export master to slave_exported.sqlite
+        self.shell = SnapshotShell(self.master_store)
+        self.shell.do_export_reference_data_and_metadata()
+
+        # Create a data_store pointing to the new slave database
+        self.slave_store = DataStore(
+            "", "", "", 0, db_name="slave_exported.sqlite", db_type="sqlite"
+        )
+
+        # Do some more imports into master
+        # We import these files: gpx_1_0_MultipleTracks.gpx
+        processor.process(
+            os.path.join(SAMPLE_DATA_PATH, "track_files", "gpx", "gpx_1_0_MultipleTracks.gpx"),
+            self.master_store,
+            False,
+        )
+
+        # Do some imports into slave
+        # We import these files: rep_test1.rep
+        # (rep_test1.rep file is the same as was imported into master earlier - this won't be in this db already
+        # as measurements are not exported)
+        processor.process(
+            os.path.join(SAMPLE_DATA_PATH, "track_files", "rep_data", "rep_test1.rep"),
+            self.slave_store,
+            False,
+        )
+
+        # Do some more imports into slave - now with a new default resolver, so we get different default values
+        # We import these files:  uk_track.rep
+        new_default_resolver = DefaultResolver()
+        new_default_resolver.default_platform_name = "PLATFORM-1-SLAVE"
+        new_default_resolver.default_trigraph = "SL1"
+        new_default_resolver.default_quadgraph = "SLT1"
         new_default_resolver.default_identifier = "123-SLAVE"
         new_default_resolver.default_platform_type = "Warship-SLAVE"
         new_default_resolver.default_nationality = "UK-SLAVE"
