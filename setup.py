fca7ed4eb744db4e8d36c298dc3e9d216b493ca5
Ian Mayo <ian@planetmayo.com>
2020-07-01 12:17:32 +0100

    Bump version: 0.0.17 → 0.0.18

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/debrief/pepys-import",
-    version="0.0.17",
+    version="version='0.0.18'",
     zip_safe=False,
 )
