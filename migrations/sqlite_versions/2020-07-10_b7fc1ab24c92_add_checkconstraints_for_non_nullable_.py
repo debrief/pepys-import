a2dbb6bd5c7064ac8e6cd9ca76f21109daf243b6
Robin Wilson <robin@rtwilson.com>
2020-07-11 16:11:08 +0100

    Use copy_from param in SQLite migration, with copied version of SQLite table definition

--- migrations/sqlite_versions/2020-07-10_b7fc1ab24c92_add_checkconstraints_for_non_nullable_.py
+++ migrations/sqlite_versions/2020-07-10_b7fc1ab24c92_add_checkconstraints_for_non_nullable_.py
@@ -5,11 +5,45 @@ Revises: d5d740c76aa3
 Create Date: 2020-07-10 13:24:56.007611
 
 """
+from datetime import datetime
+from uuid import uuid4
+
 import geoalchemy2
 import sqlalchemy as sa
 from alembic import op
+from geoalchemy2 import Geometry
+from sqlalchemy import DATE, Boolean, Column, DateTime, ForeignKey, Integer, MetaData, String, Text
+from sqlalchemy.dialects.sqlite import REAL, TIMESTAMP
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import deferred
+from sqlalchemy.sql.schema import CheckConstraint
 
 import pepys_import
+from pepys_import.core.store import constants
+from pepys_import.core.store.common_db import (
+    ActivationMixin,
+    CommentMixin,
+    ContactMixin,
+    DatafileMixin,
+    ElevationPropertyMixin,
+    GeometryMixin,
+    HostedByMixin,
+    LocationPropertyMixin,
+    LogMixin,
+    LogsHoldingMixin,
+    MediaMixin,
+    ParticipantMixin,
+    PlatformMixin,
+    ReferenceRepr,
+    SensorMixin,
+    StateMixin,
+    SynonymMixin,
+    TaggedItemMixin,
+    TaskMixin,
+)
+from pepys_import.core.store.db_base import sqlite_naming_convention
+from pepys_import.core.store.db_status import TableTypes
+from pepys_import.utils.sqlalchemy_utils import UUIDType
 
 # revision identifiers, used by Alembic.
 revision = "b7fc1ab24c92"
@@ -86,7 +120,31 @@ def upgrade():
 
 
 def downgrade():
-    with op.batch_alter_table("Tasks", schema=None) as batch_op:
+    Metadata = MetaData(naming_convention=sqlite_naming_convention)
+    BaseSpatiaLite = declarative_base(metadata=Metadata)
+
+    class Task(BaseSpatiaLite, TaskMixin):
+        __tablename__ = constants.TASK
+        table_type = TableTypes.METADATA
+        table_type_id = 4
+
+        task_id = Column(UUIDType, primary_key=True, default=uuid4)
+        name = Column(
+            String(150), CheckConstraint("name <> ''", name="ck_Tasks_name"), nullable=False
+        )
+        parent_id = Column(
+            UUIDType, ForeignKey("Tasks.task_id", onupdate="cascade"), nullable=False
+        )
+        start = Column(TIMESTAMP, nullable=False)
+        end = Column(TIMESTAMP, nullable=False)
+        environment = deferred(Column(String(150)))
+        location = deferred(Column(String(150)))
+        privacy_id = Column(
+            UUIDType, ForeignKey("Privacies.privacy_id", onupdate="cascade"), nullable=False
+        )
+        created_date = Column(DateTime, default=datetime.utcnow)
+
+    with op.batch_alter_table("Tasks", schema=None, copy_from=Task.__table__) as batch_op:
         batch_op.drop_constraint("ck_Tasks_name", type_="check")
 
     with op.batch_alter_table("Synonyms", schema=None) as batch_op:
