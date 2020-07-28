4fda06268e28b51048fe830a4952d33682897cf2
Robin Wilson <robin@rtwilson.com>
2020-07-28 08:24:50 +0100

    Mark parts to be skipped for coverage as platform-specific and tests only run on Linux

--- pepys_import/utils/sqlite_utils.py
+++ pepys_import/utils/sqlite_utils.py
@@ -4,9 +4,9 @@ import platform
 SYSTEM = platform.system()
 if SYSTEM == "Linux":
     PLATFORM_EXTENSION_PATH = "mod_spatialite"
-elif SYSTEM == "Darwin":  # Darwin is MacOS
+elif SYSTEM == "Darwin":  # Darwin is MacOS  pragma: no cover (tests only run on Linux)
     PLATFORM_EXTENSION_PATH = "/usr/local/lib/mod_spatialite.dylib"
-elif SYSTEM == "Windows":
+elif SYSTEM == "Windows":  # pragma: no cover (tests only run on Linux)
     PLATFORM_EXTENSION_PATH = "mod_spatialite"
 
 
