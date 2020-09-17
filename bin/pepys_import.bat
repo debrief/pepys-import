d6826158b4bb1be6b254a0921973afb45d1360dd
Robin Wilson <robin@rtwilson.com>
2020-09-17 10:00:25 +0100

    Add error-handling to batch files that are used to run Pepys
    This consists of error-handling for set_paths.bat, where errors may occur if paths aren't available or set properly, and then code to catch the errors from set_paths.bat in the other batch files (like pepys_import.bat, pepys_admin.bat and so on).

--- bin/pepys_import.bat
+++ bin/pepys_import.bat
@@ -1,3 +1,12 @@
 @echo off
 CALL set_paths.bat
+REM If error returned from set_paths.bat then don't continue with running python
+IF ERRORLEVEL 1 GOTO :ERROR
 python -m pepys_import.cli %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+REM Go to the end of the file, skipping the :ERROR label below
+GOTO :eof
+
+REM PAUSE so that the user can see the error
+:ERROR
+PAUSE
