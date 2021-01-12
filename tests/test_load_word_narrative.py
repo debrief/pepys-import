9353cdb3d52db038337cd4cabc87a61c3df21f79
Robin Wilson <robin@rtwilson.com>
2021-01-12 09:57:11 +0000

    Add outline test for files with no hidden text

--- tests/test_load_word_narrative.py
+++ tests/test_load_word_narrative.py
@@ -7,7 +7,10 @@ from pepys_import.core.store.data_store import DataStore
 from pepys_import.file.file_processor import FileProcessor
 
 FILE_PATH = os.path.dirname(__file__)
-DATA_PATH = os.path.join(FILE_PATH, "sample_data/track_files/word/Narrative Example.docx")
+FULL_NARRATIVE_PATH = os.path.join(FILE_PATH, "sample_data/track_files/word/Narrative Example.docx")
+NO_HIDDEN_TEXT_PATH = os.path.join(
+    FILE_PATH, "sample_data/track_files/word/NarrativeExample_NoHiddenText.docx"
+)
 
 
 class TestLoadWordNarrative(unittest.TestCase):
@@ -19,7 +22,7 @@ class TestLoadWordNarrative(unittest.TestCase):
         pass
 
     @patch("pepys_import.core.store.common_db.prompt", return_value="2")
-    def test_load_word_data(self, patched_prompt):
+    def test_load_word_data_full_narrative(self, patched_prompt):
         processor = FileProcessor(archive=False)
         processor.register_importer(WordNarrativeImporter())
 
@@ -38,7 +41,66 @@ class TestLoadWordNarrative(unittest.TestCase):
             self.assertEqual(len(datafiles), 0)
 
         # parse the file
-        processor.process(DATA_PATH, self.store, False)
+        processor.process(FULL_NARRATIVE_PATH, self.store, False)
+
+        # # check data got created
+        # with self.store.session_scope():
+        #     # there must be states after the import
+        #     states = self.store.session.query(self.store.db_classes.State).all()
+        #     self.assertEqual(len(states), 746)
+
+        #     # there must be platforms after the import
+        #     platforms = self.store.session.query(self.store.db_classes.Platform).all()
+        #     self.assertEqual(len(platforms), 5)
+
+        #     # there must be one datafile afterwards
+        #     datafiles = self.store.session.query(self.store.db_classes.Datafile).all()
+        #     self.assertEqual(len(datafiles), 6)
+
+        #     # There should be one state with no elevation, which comes from the NaN
+        #     # in the elevation field in the first line of uk_track.rep
+        #     states_with_no_elevation = (
+        #         self.store.session.query(self.store.db_classes.State)
+        #         .filter(self.store.db_classes.State.elevation.is_(None))
+        #         .all()
+        #     )
+
+        #     assert len(states_with_no_elevation) == 1
+
+        #     # This state should have a time of
+        #     assert states_with_no_elevation[0].time == datetime.datetime(2018, 5, 7, 5, 0, 0)
+
+        #     # there should be 581 points with an elevation of 0m
+        #     # (this proves that zero values are imported properly and not
+        #     # treated as errors)
+        #     elev_zero_states = (
+        #         self.store.session.query(self.store.db_classes.State)
+        #         .filter(self.store.db_classes.State.elevation == 0)
+        #         .all()
+        #     )
+        #     assert len(elev_zero_states) == 581
+
+    @patch("pepys_import.core.store.common_db.prompt", return_value="2")
+    def test_load_word_data_no_hidden_text(self, patched_prompt):
+        processor = FileProcessor(archive=False)
+        processor.register_importer(WordNarrativeImporter())
+
+        # check states empty
+        with self.store.session_scope():
+            # there must be no states at the beginning
+            states = self.store.session.query(self.store.db_classes.State).all()
+            self.assertEqual(len(states), 0)
+
+            # there must be no platforms at the beginning
+            platforms = self.store.session.query(self.store.db_classes.Platform).all()
+            self.assertEqual(len(platforms), 0)
+
+            # there must be no datafiles at the beginning
+            datafiles = self.store.session.query(self.store.db_classes.Datafile).all()
+            self.assertEqual(len(datafiles), 0)
+
+        # parse the file
+        processor.process(NO_HIDDEN_TEXT_PATH, self.store, False)
 
         # # check data got created
         # with self.store.session_scope():
