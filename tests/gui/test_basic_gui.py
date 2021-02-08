649e4a989643141d9374e78252f73def6c407a35
Robin Wilson <robin@rtwilson.com>
2021-02-08 18:55:02 +0000

    Skip GUI tests if pytest was run without -s option

--- tests/gui/test_basic_gui.py
+++ tests/gui/test_basic_gui.py
@@ -5,10 +5,12 @@ import pytest
 from tests.gui.gui_test_utils import run_gui
 
 pytestmark = pytest.mark.skipif(sys.platform == "win32", reason="Don't run on Windows")
-pytestmark = pytest.mark.skipif(sys.platform == "win32", reason="Don't run on Windows")
 
 
-def test_gui_opens():
+def test_gui_opens(pytestconfig):
+    if pytestconfig.getoption("capture") != "no":
+        pytest.skip("Skipped because pytest was not run with -s option")
+
     result = run_gui(print_output=True)
 
     assert "Build filters  F3" in result
@@ -16,13 +18,19 @@ def test_gui_opens():
     assert "Platform    ▼" in result
 
 
-def test_gui_help():
+def test_gui_help(pytestconfig):
+    if pytestconfig.getoption("capture") != "no":
+        pytest.skip("Skipped because pytest was not run with -s option")
+
     result = run_gui(keys=b"\x1bOP")  # Escape sequence for F1
 
     assert "─| Help |─" in result
 
 
-def test_gui_field_selection():
+def test_gui_field_selection(pytestconfig):
+    if pytestconfig.getoption("capture") != "no":
+        pytest.skip("Skipped because pytest was not run with -s option")
+
     result = run_gui(keys=b"\x06")  # Escape sequence for Ctrl-F
 
     assert "──| Select fields |──" in result
