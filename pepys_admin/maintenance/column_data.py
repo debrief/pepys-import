033af08757fc074d424c628dea9af1dc3a95701e
Robin Wilson <robin@rtwilson.com>
2021-02-18 01:38:21 +0000

    Generate the column data all in one session, rather than opening and closing multiple sessions

--- pepys_admin/maintenance/column_data.py
+++ pepys_admin/maintenance/column_data.py
@@ -81,20 +81,21 @@ def create_column_data(data_store, table_object, set_percentage=None):
     iteration_perc = 100.0 / total_iterations
     i = 1
 
-    column_data = {}
-    for col in cols:
-        sys_name = col.key
-        # Deal with columns where the actual column member
-        # variable is something like `_speed`, because we're
-        # using a `speed` property to access the column
-        if sys_name.startswith("_"):
-            sys_name = sys_name[1:]
+    with data_store.session_scope():
+        column_data = {}
+        for col in cols:
+            sys_name = col.key
+            # Deal with columns where the actual column member
+            # variable is something like `_speed`, because we're
+            # using a `speed` property to access the column
+            if sys_name.startswith("_"):
+                sys_name = sys_name[1:]
 
-        details = {"type": get_type_name(col.type), "system_name": sys_name}
+            details = {"type": get_type_name(col.type), "system_name": sys_name}
 
-        if details["type"] in ["string", "id"]:
-            # Get values
-            with data_store.session_scope():
+            if details["type"] in ["string", "id"]:
+                # Get values
+
                 all_records = data_store.session.query(table_object).all()
                 values = [
                     str_if_not_none(getattr(record, details["system_name"]))
@@ -102,44 +103,43 @@ def create_column_data(data_store, table_object, set_percentage=None):
                 ]
                 details["values"] = sorted(remove_duplicates_and_nones(values))
 
-        column_data[get_display_name(sys_name)] = details
+            column_data[get_display_name(sys_name)] = details
 
-        if callable(set_percentage):
-            set_percentage(i * iteration_perc)
-        i += 1
+            if callable(set_percentage):
+                set_percentage(i * iteration_perc)
+            i += 1
 
-    for ap_name, ap_obj in zip(assoc_proxy_names, assoc_proxy_objs):
-        ap_type = getattr(ap_obj.target_class, ap_obj.value_attr).type
+        for ap_name, ap_obj in zip(assoc_proxy_names, assoc_proxy_objs):
+            ap_type = getattr(ap_obj.target_class, ap_obj.value_attr).type
 
-        details = {"type": get_type_name(ap_type), "system_name": ap_name}
+            details = {"type": get_type_name(ap_type), "system_name": ap_name}
 
-        if details["type"] in ["string", "id"]:
-            if details["system_name"] == "nationality_name":
-                # Get nationality names as a special case, as we want to sort by
-                # priority
+            if details["type"] in ["string", "id"]:
+                if details["system_name"] == "nationality_name":
+                    # Get nationality names as a special case, as we want to sort by
+                    # priority
 
-                # nullslast in the expression below makes NULL entries appear at the end
-                # of the sorted list - if we don't have this then they sort as 'zero'
-                # and come before the prioritised ones
-                all_nationalities = (
-                    data_store.session.query(data_store.db_classes.Nationality)
-                    .order_by(nullslast(data_store.db_classes.Nationality.priority.asc()))
-                    .all()
-                )
-                nationality_names = [nationality.name for nationality in all_nationalities]
-                details["values"] = nationality_names
-            elif details["system_name"] == "privacy_name":
-                # Get privacy names as a special case, as we want to sort by level
-                all_privacies = (
-                    data_store.session.query(data_store.db_classes.Privacy)
-                    .order_by(data_store.db_classes.Privacy.level)
-                    .all()
-                )
-                privacy_names = [priv.name for priv in all_privacies]
-                details["values"] = privacy_names
-            else:
-                # For all other columns, no special processing is needed
-                with data_store.session_scope():
+                    # nullslast in the expression below makes NULL entries appear at the end
+                    # of the sorted list - if we don't have this then they sort as 'zero'
+                    # and come before the prioritised ones
+                    all_nationalities = (
+                        data_store.session.query(data_store.db_classes.Nationality)
+                        .order_by(nullslast(data_store.db_classes.Nationality.priority.asc()))
+                        .all()
+                    )
+                    nationality_names = [nationality.name for nationality in all_nationalities]
+                    details["values"] = nationality_names
+                elif details["system_name"] == "privacy_name":
+                    # Get privacy names as a special case, as we want to sort by level
+                    all_privacies = (
+                        data_store.session.query(data_store.db_classes.Privacy)
+                        .order_by(data_store.db_classes.Privacy.level)
+                        .all()
+                    )
+                    privacy_names = [priv.name for priv in all_privacies]
+                    details["values"] = privacy_names
+                else:
+                    # For all other columns, no special processing is needed
                     all_records = data_store.session.query(ap_obj.target_class).all()
                     values = [
                         str_if_not_none(getattr(record, ap_obj.value_attr))
@@ -147,7 +147,7 @@ def create_column_data(data_store, table_object, set_percentage=None):
                     ]
                     details["values"] = sorted(remove_duplicates_and_nones(values))
 
-        column_data[get_display_name(ap_name)] = details
+            column_data[get_display_name(ap_name)] = details
 
         if callable(set_percentage):
             set_percentage(i * iteration_perc)
