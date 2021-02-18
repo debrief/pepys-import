191f1e36eda55d5d39a99ed47550d1687bf5ffac
Robin Wilson <robin@rtwilson.com>
2021-02-18 01:11:43 +0000

    Remove test for Select Fields dialog, as it won't open without a table selected
    (I can't get selecting a table to work on the interactive tests - we can investigate this
    more later if needed)

--- tests/gui_tests/test_gui_interactively.py
+++ tests/gui_tests/test_gui_interactively.py
@@ -37,12 +37,3 @@ def test_gui_help(pytestconfig):
     result = run_gui(keys=b"\x1bOP")  # Escape sequence for F1
 
     assert "─| Help |─" in result
-
-
-def test_gui_field_selection(pytestconfig):
-    if pytestconfig.getoption("capture") != "no":
-        pytest.skip("Skipped because pytest was not run with -s option")
-
-    result = run_gui(keys=b"\x06")  # Escape sequence for Ctrl-F
-
-    assert "──| Select fields |──" in result
