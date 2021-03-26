2f6020e98464c431237cbe760d3dfe568e6cc9c7
Robin Wilson <robin@rtwilson.com>
2021-03-26 19:11:06 +0000

    Fix migration tests, as import won't work without exercise field in Tasks table

--- tests/migration_tests/test_migration.py
+++ tests/migration_tests/test_migration.py
@@ -196,14 +196,14 @@ class StepByStepMigrationTestCase(unittest.TestCase):
         # The following dictionaries are going to be used to import datafiles. When the version of
         # the database is sufficient (if the version is the same with a key in dictionary)
         self.sqlite_version_datafile_dict = {
-            "e9049ca0338a": [
+            "6754c33dca6d": [
                 "nisida_example.txt",
                 "nisida_invalid_header_line.txt",
                 "nisida_split_narrative.txt",
             ]
         }
         self.postgres_version_datafile_dict = {
-            "0f8d24a830d0": [
+            "004bd2e9fce4": [
                 "nisida_example.txt",
                 "nisida_invalid_header_line.txt",
                 "nisida_split_narrative.txt",
