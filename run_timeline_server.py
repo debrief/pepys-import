a7ddcf427ea91ef423ea8ddd5d14083f434ab71b
Robin Wilson <robin@rtwilson.com>
2021-05-06 09:50:23 +0100

    Add Procfile for Heroku

---
+++ run_timeline_server.py
@@ -0,0 +1,8 @@
+import os
+
+from pepys_timeline.app import create_app
+
+if __name__ == "__main__":
+    app = create_app()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
