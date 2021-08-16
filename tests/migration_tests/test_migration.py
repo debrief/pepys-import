db35e17eb41c9b97944d877322fc81e1411e6923
Robin Wilson <robin@rtwilson.com>
2021-08-16 11:22:11 +0100

    Remove incorrect use of text()

--- tests/migration_tests/test_migration.py
+++ tests/migration_tests/test_migration.py
@@ -139,9 +139,9 @@ class MigratePostgresTestCase(unittest.TestCase):
 
 def get_alembic_version(connection, db_type="sqlite"):
     if db_type == "sqlite":
-        version = connection.execute(text("SELECT version_num FROM alembic_version;"))
+        version = connection.execute("SELECT version_num FROM alembic_version;")
     elif db_type == "postgres":
-        version = connection.execute(text('SELECT version_num FROM pepys."alembic_version";'))
+        version = connection.execute('SELECT version_num FROM pepys."alembic_version";')
     else:
         print("Given DB type is wrong!")
         return
