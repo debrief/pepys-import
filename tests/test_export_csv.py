046f88d8b4d028a0edec586270bfbbe509a17fec
Robin Wilson <robin@rtwilson.com>
2021-09-14 10:17:34 +0100

    Test exporting state CSV

--- tests/test_export_csv.py
+++ tests/test_export_csv.py
@@ -5,7 +5,13 @@ from pathlib import Path
 
 import pytest
 
+from importers.gpx_importer import GPXImporter
+from importers.replay_importer import ReplayImporter
 from pepys_import.core.store.data_store import DataStore
+from pepys_import.file.file_processor import FileProcessor
+
+REP_FILE = os.path.join(os.path.dirname(__file__), "sample_data/track_files/rep_data/uk_track.rep")
+GPX_FILE = os.path.join(os.path.dirname(__file__), "sample_data/track_files/gpx/gpx_1_0.gpx")
 
 
 def read_csv(filename):
@@ -125,7 +131,6 @@ def test_export_object_to_csv_incorrect_header():
 
 
 def test_export_platform_type():
-    # Generate the file and populate the data within it
     filename = str(tempfile.gettempdir()) + "/output.csv"
     columns = ["platform_type_id", "name", "default_data_interval_secs"]
     ds = DataStore("", "", "", 0, ":memory:", db_type="sqlite")
@@ -156,6 +161,97 @@ def test_export_platform_type():
             objects[i].default_data_interval_secs
         )
         assert export_contents[i]["platform_type_id"] == str(objects[i].platform_type_id)
+
+    # Remove the file that was made
+    os.remove(filename)
+
+
+def test_export_state():
+    filename = str(tempfile.gettempdir()) + "/output.csv"
+    columns = [
+        "state_id",
+        "time",
+        "location",
+        "elevation",
+        "heading",
+        "course",
+        "speed",
+        "remarks",
+        "created_date",
+        "platform_name",
+        "privacy_name",
+        "sensor_name",
+        "source_reference",
+    ]
+    ds = DataStore("", "", "", 0, ":memory:", db_type="sqlite")
+    ds.initialise()
+
+    with ds.session_scope():
+        ds.populate_reference()
+        ds.populate_metadata()
+
+        processor = FileProcessor(archive=False)
+        processor.register_importer(ReplayImporter())
+        processor.register_importer(GPXImporter())
+
+        processor.process(
+            REP_FILE,
+            ds,
+            False,
+        )
+        processor.process(
+            GPX_FILE,
+            ds,
+            False,
+        )
+
+    with ds.session_scope():
+        entries = ds.session.query(ds.db_classes.State).all()
+        ids = [entry.state_id for entry in entries]
+
+    ds.export_objects_to_csv(
+        ds.db_classes.State,
+        ids,
+        columns,
+        filename,
+    )
+
+    objects = ds.convert_ids_to_objects(ids, ds.db_classes.State)
+
+    export_contents = read_csv(filename)
+
+    expected_cols = [
+        "state_id",
+        "time",
+        "location_lat (decimal degrees)",
+        "location_lon (decimal degrees)",
+        "elevation (meter)",
+        "heading (degree)",
+        "course (degree)",
+        "speed (meter / second)",
+        "remarks",
+        "created_date",
+        "platform_name",
+        "privacy_name",
+        "sensor_name",
+        "source_reference",
+    ]
+    assert set(export_contents[0].keys()) == set(expected_cols)
+
+    for i in range(1, len(objects)):
+        assert export_contents[i]["state_id"] == str(objects[i].state_id)
+        assert export_contents[i]["time"] == str(objects[i].time)
+        if objects[i].heading is not None:
+            assert export_contents[i]["heading (degree)"] == str(objects[i].heading.magnitude)
+        else:
+            assert export_contents[i]["heading (degree)"] == ""
+        assert export_contents[i]["platform_name"] == str(objects[i].platform_name)
+        assert export_contents[i]["location_lat (decimal degrees)"] == str(
+            objects[i].location.latitude
+        )
+        assert export_contents[i]["location_lon (decimal degrees)"] == str(
+            objects[i].location.longitude
+        )
 
     # Remove the file that was made
     os.remove(filename)
