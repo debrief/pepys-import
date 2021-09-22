d12d603763977bd1c3e0805009911e1c10a8967e
Matt Willis <mew@nsc.co.uk>
2021-09-22 15:47:37 +0100

    Remaining boilerplate for the importer

--- importers/link_16_importer.py
+++ importers/link_16_importer.py
@@ -11,17 +11,36 @@ class Link16Importer(Importer):
             datafile_type="Link-16"
         )
 
-        ''' 
-        Link-16 has (at least?) two versions that MWC would like to import. 
-        Looking at MWC parser, we're expecting to extract the following fields (present in both versions):
-          - Name {v1: [2], v2: [4]}
-          - Symbol?
-          - Lat {v1: [8], v2: [13]}
-          - Lon {v1: [7], v2: [14]} # Yes, they did swap Lat/lon between versions...
-          - Course {v1: [11], v2: [16]} - deg
-          - Speed {v1: [12], v2: [17]} - ft
-          - Depth (Altitude) {v1: [10], v2: [15]} - ft
+    def can_load_this_type(self, suffix):
+        return suffix.upper() == ".CSV"
 
-          We also have a date/time - which is offset from a user input start date/time. 
-          This date time is in position [1] in both versions of the file
-        '''
+    def can_load_this_filename(self, filename):
+        return True
+    
+    def can_load_this_header(self, header):
+        # V1 starts w/ PPLI
+        # V2 starts w/ Xmt/Rcv
+        return header.startswith("PPLI") or header.startswith("Xmt/Rcv")
+
+    def can_load_this_file(self, file_contents):
+        return True
+
+    def _load_this_line(self, data_store, line_number, line, datafile, change_id):
+        # Skip the header
+        if line_number == 1:
+            return
+
+    ''' 
+    Link-16 has (at least?) two versions that MWC would like to import. 
+    Looking at MWC parser, we're expecting to extract the following fields (present in both versions):
+        - Name {v1: [2], v2: [4]}
+        - Symbol?
+        - Lat {v1: [8], v2: [13]}
+        - Lon {v1: [7], v2: [14]} # Yes, they did swap Lat/lon between versions...
+        - Course {v1: [11], v2: [16]} - deg
+        - Speed {v1: [12], v2: [17]} - ft
+        - Depth (Altitude) {v1: [10], v2: [15]} - ft
+
+        We also have a date/time - which is offset from a user input start date/time. 
+        This date time is in position [1] in both versions of the file
+    '''
