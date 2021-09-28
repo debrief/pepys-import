a8db6b1e1cee336860aa0fb38bef4cab1f5808bb
Matt Willis <88482857+mew-nsc@users.noreply.github.com>
2021-09-28 16:49:53 +0100

    WIP

--- importers/link_16_importer.py
+++ importers/link_16_importer.py
@@ -1,5 +1,13 @@
+# from pepys_import.core.formats import unit_registry
+# from pepys_import.core.formats.location import Location
 from pepys_import.core.validators import constants
 from pepys_import.file.importer import Importer
+
+# from pepys_import.file.highlighter.support.combine import combine_tokens
+# from pepys_import.utils.unit_utils import convert_absolute_angle, convert_distance, convert_speed
+
+V1_HEADER = "PPLI"
+V2_HEADER = "Xmt/Rcv"
 
 
 class Link16Importer(Importer):
@@ -26,21 +34,76 @@ class Link16Importer(Importer):
     def can_load_this_header(self, header):
         # V1 starts w/ PPLI
         # V2 starts w/ Xmt/Rcv
-        return header.startswith("PPLI") or header.startswith("Xmt/Rcv")
+        return header.startswith(V1_HEADER) or header.startswith(V2_HEADER)
 
     def can_load_this_file(self, file_contents):
         return True
 
     def _load_this_line(self, data_store, line_number, line, datafile, change_id):
-        # Skip the header
         if line_number == 1:
+            # Will only parse if one of these headers found
+            self.version = 1 if line.startswith(V1_HEADER) else 2
             return
         tokens = line.tokens(line.CSV_TOKENISER, ",")
+        if len(tokens) <= 1:
+            # To skip over any blank lines
+            return
 
-        track_number = tokens[2] if self.version == 1 else tokens[4]
+        if self.version == 1:
+            if len(tokens) < 15:
+                self.errors.append(
+                    {self.error_type: f"Error on line {line_number}. Not enough tokens: {line}"}
+                )
+                return
 
-    """ 
-    Link-16 has (at least?) two versions that MWC would like to import. 
+            # lat_degrees_token = tokens[8]
+            # lon_degrees_token = tokens[7]
+            # heading_token = tokens[11]
+            # speed_token = tokens[12]
+            # altitude_token = tokens[10]
+
+        elif self.version == 2:
+            raise NotImplementedError()
+
+        # state = datafile.create_state(data_store, platform, sensor, timestamp, self.short_name)
+
+        # location = Location(errors=self.errors, error_type=self.error_type)
+        # lat_success = location.set_latitude_decimal_degrees(lat_degrees_token.text)
+        # lon_success = location.set_longitude_decimal_degrees(lon_degrees_token.text)
+        # if lat_success and lon_success:
+        # state.location = location
+        # combine_tokens(lon_degrees_token, lat_degrees_token).record(
+        #     self.name, "location", state.location, "decimal degrees"
+        # )
+
+        # elevation_valid, elevation = convert_distance(
+        #     altitude_token.text, unit_registry.foot, line_number, self.errors, self.error_type
+        # )
+
+        # if elevation_valid:
+        #     state.elevation = elevation
+        #     altitude_token.record(self.name, "altitude", state.elevation)
+
+        # heading_valid, heading = convert_absolute_angle(
+        #     heading_token.text, line_number, self.errors, self.error_type
+        # )
+        # if heading_valid:
+        #     state.heading = heading
+        #     heading_token.record(self.name, "heading", heading)
+
+        # speed_valid, speed = convert_speed(
+        #     speed_token.text,
+        #     unit_registry.feet_per_second,
+        #     line_number,
+        #     self.errors,
+        #     self.error_type,
+        # )
+        # if speed_valid:
+        #     state.speed = speed
+        #     speed_token.record(self.name, "speed", speed)
+
+    """
+    Link-16 has (at least?) two versions that MWC would like to import.
     Looking at MWC parser, we're expecting to extract the following fields (present in both versions):
         - Name {v1: [2], v2: [4]}
         - Symbol?
@@ -50,6 +113,6 @@ class Link16Importer(Importer):
         - Speed {v1: [12], v2: [17]} - ft
         - Depth (Altitude) {v1: [10], v2: [15]} - ft
 
-        We also have a date/time - which is offset from a user input start date/time. 
+        We also have a date/time - which is offset from a user input start date/time.
         This date time is in position [1] in both versions of the file
     """
--- tests/test_load_link16.py
+++ tests/test_load_link16.py
@@ -1,8 +1,19 @@
+# import datetime
+import os
 import unittest
 
 from importers.link_16_importer import Link16Importer
 from pepys_import.core.store.data_store import DataStore
 from pepys_import.file.file_processor import FileProcessor
+
+FILE_PATH = os.path.dirname(__file__)
+DATA_PATH_V1 = os.path.join(
+    FILE_PATH, "sample_data/track_files/Link16/Sample.Data.GEV.Link.16.V1.csv"
+)
+DATA_PATH_V2 = os.path.join(
+    FILE_PATH, "sample_data/track_files/Link16/Sample.Data.GEV.Link.16.V2.csv"
+)
+
 
 class TestLoadLink16(unittest.TestCase):
     def setUp(self):
@@ -14,8 +25,60 @@ class TestLoadLink16(unittest.TestCase):
 
     def test_process_link16_v1_data(self):
         processor = FileProcessor(archive=False)
-        processor.register_importer(Link16Importer)
-        
+        processor.register_importer(Link16Importer())
+
+        # check states empty
+        with self.store.session_scope():
+            # there must be no states at the beginning
+            states = self.store.session.query(self.store.db_classes.State).all()
+            self.assertEqual(len(states), 0)
+
+            # there must be no platforms at the beginning
+            platforms = self.store.session.query(self.store.db_classes.Platform).all()
+            self.assertEqual(len(platforms), 0)
+
+            # there must be no datafiles at the beginning
+            datafiles = self.store.session.query(self.store.db_classes.Datafile).all()
+            self.assertEqual(len(datafiles), 0)
+
+        # parse the data
+        processor.process(DATA_PATH_V1, self.store, False)
+
+        # check data got created
+        with self.store.session_scope():
+            # there must be states after the import
+            states = self.store.session.query(self.store.db_classes.State).all()
+            self.assertEqual(len(states), 8)
+
+    #         # there must be platforms after the import
+    #         platforms = self.store.session.query(self.store.db_classes.Platform).all()
+    #         self.assertEqual(len(platforms), 18)
+
+    #         # there must be one datafile afterwards
+    #         datafiles = self.store.session.query(self.store.db_classes.Datafile).all()
+    #         self.assertEqual(len(datafiles), 1)
+
+    #         # Check that there is an elevation of 147 reported (test file was manually edited
+    #         # to contain an elevation of 147m)
+    #         results = (
+    #             self.store.session.query(self.store.db_classes.State)
+    #             .filter(self.store.db_classes.State.elevation == 147)
+    #             .all()
+    #         )
+    #         assert len(results) == 1
+    #         assert results[0].time == datetime.datetime(2019, 8, 6, 4, 40, 0)
 
 
-    
+""" Things to test:
+        Parse the date/time stamp from the name
+        Roll timestamps forward correctly
+        We can read v1 format
+        We can read v2 format
+        Check incorrect # of tokens for v1 format throws error
+        Check incorrect # of tokens for v2 format throws error
+
+"""
+
+
+if __name__ == "__main__":
+    unittest.main()
