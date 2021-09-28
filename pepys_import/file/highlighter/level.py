0e36892613dbd04320e2cec9b450c8a18ec4d619
Ian Mayo <ian@planetmayo.com>
2021-09-28 14:10:04 +0000

    Try to improve formatting, to keep black happy

--- pepys_import/file/highlighter/level.py
+++ pepys_import/file/highlighter/level.py
@@ -1,10 +1,10 @@
 from enum import Enum
 
 class HighlightLevel(Enum):
-    """ The level of recording highlighted extractions """
+    """The level of recording highlighted extractions"""
     NONE = 1
-    """ No highlighting or recording of extractions """
+    """No highlighting or recording of extractions"""
     HTML = 2
-    """ Produce a highlighted HTML file showing the extractions"""
+    """Produce a highlighted HTML file showing the extractions"""
     DATABASE = 3
-    """ Produce a highlighted HTML file and record extractions to the database"""
+    """Produce a highlighted HTML file and record extractions to the database"""
