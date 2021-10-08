7800daea4fef03215193c4264891c04559eded8a
Robin Wilson <robin@rtwilson.com>
2021-10-08 22:10:55 +0100

    Add back in other test

--- tests/gui_tests/test_gui_interactively.py
+++ tests/gui_tests/test_gui_interactively.py
@@ -122,44 +122,44 @@ async def test_open_help_dialog(test_datastore):
         await task
 
 
-# async def test_select_platform_type(test_datastore):
-#     # Test application in a dummy session.
-#     input = DummyInput()
-#     output = DummyOutput()
-#     # output = None
+async def test_select_platform_type(test_datastore):
+    # Test application in a dummy session.
+    input = DummyInput()
+    output = DummyOutput()
+    # output = None
 
-#     with create_app_session(output=output, input=input):
-#         gui = MaintenanceGUI(test_datastore)
+    with create_app_session(output=output, input=input):
+        gui = MaintenanceGUI(test_datastore)
 
-#         task = asyncio.create_task(gui.app.run_async())
+        task = asyncio.create_task(gui.app.run_async())
 
-#         ready_event = asyncio.Event()
-#         gui.app.after_render += lambda _: ready_event.set()
-#         await ready_event.wait()
-#         ready_event.clear()
+        ready_event = asyncio.Event()
+        gui.app.after_render += lambda _: ready_event.set()
+        await ready_event.wait()
+        ready_event.clear()
 
-#         # Type 'PlatformTy' - which filters a dropdown box on every keypress
-#         for letter in "PlatformTy":
-#             gui.app.key_processor.feed(KeyPress(letter))
-#             gui.app.key_processor.process_keys()
+        # Type 'PlatformTy' - which filters a dropdown box on every keypress
+        for letter in "PlatformTy":
+            gui.app.key_processor.feed(KeyPress(letter))
+            gui.app.key_processor.process_keys()
 
-#             await ready_event.wait()
-#             ready_event.clear()
+            await ready_event.wait()
+            ready_event.clear()
 
-#         # Send Enter to select the 'PlatformTypes' entry
-#         gui.app.key_processor.feed(KeyPress(Keys.ControlM, "\r"))
-#         gui.app.key_processor.process_keys()
+        # Send Enter to select the 'PlatformTypes' entry
+        gui.app.key_processor.feed(KeyPress(Keys.ControlM, "\r"))
+        gui.app.key_processor.process_keys()
 
-#         await ready_event.wait()
-#         ready_event.clear()
+        await ready_event.wait()
+        ready_event.clear()
 
-#         assert gui.current_table_object == test_datastore.db_classes.PlatformType
+        assert gui.current_table_object == test_datastore.db_classes.PlatformType
 
-#         # Send Enter to say yes
-#         gui.app.exit()
+        # Send Enter to say yes
+        gui.app.exit()
 
-#         # Wait for the application to properly terminate.
-#         await task
+        # Wait for the application to properly terminate.
+        await task
 
 
 # async def test_show_help(test_datastore):
