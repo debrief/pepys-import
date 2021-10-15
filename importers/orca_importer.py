442569becb4ea771c8f05f1a1d552a2ed2013898
Matt Willis <88482857+mew-nsc@users.noreply.github.com>
2021-10-15 15:45:08 +0100

    Extract the platform

--- importers/orca_importer.py
+++ importers/orca_importer.py
@@ -1,4 +1,9 @@
+from datetime import datetime
+
+from tqdm import tqdm
+
 from pepys_import.core.validators import constants
+from pepys_import.file.highlighter.xml_parser import parse
 from pepys_import.file.importer import Importer
 
 
@@ -34,4 +39,37 @@ class OrcaImporter(Importer):
         # - Read in the file
         # - Parse the platform
         # - Parse the tracks
-        pass
+
+        # Re-read the data file with the Pepys xml parser
+        try:
+            doc = parse(path, highlighted_file=file_object)
+        except Exception as e:
+            self.errors.append(
+                {self.error_type: f'Invalid ORCA file at {path}\nError from parsing was "{str(e)}"'}
+            )
+            return
+        # Now start parsing
+        for mission in tqdm(doc.findall(".//{*}mission")):
+            platform_name_element = mission.find("{*}unitname")
+            platform_name = platform_name_element.text
+            platform = self.get_cached_platform(
+                data_store, platform_name=platform_name, change_id=change_id
+            )
+
+            sensor_type = data_store.add_to_sensor_types("Location-ORCA", change_id=change_id).name
+            sensor = platform.get_sensor(
+                data_store=data_store,
+                sensor_name="ORCA",  # TODO - should this also be GPS?
+                sensor_type=sensor_type,
+                change_id=change_id,
+            )
+
+            # TODO - get timestamp from the actual data
+            # Note that example only has a "creation time" for a track
+
+            timestamp = datetime(2021, 10, 15, 1, 2, 3)
+            state = datafile.create_state(data_store, platform, sensor, timestamp, self.short_name)
+
+            print(state)
+
+            datafile.flush_extracted_tokens()
--- tests/test_load_orca.py
+++ tests/test_load_orca.py
@@ -1,6 +1,12 @@
+import os
 import unittest
 
+from importers.orca_importer import OrcaImporter
 from pepys_import.core.store.data_store import DataStore
+from pepys_import.file.file_processor import FileProcessor
+
+FILE_PATH = os.path.dirname(__file__)
+DATA_PATH = os.path.join(FILE_PATH, "sample_data/track_files/Orca/Orca_Mission.xml")
 
 
 class TestLoadOrca(unittest.TestCase):
@@ -10,3 +16,42 @@ class TestLoadOrca(unittest.TestCase):
 
     def tearDown(self):
         pass
+
+    def test_load_orca(self):
+        processor = FileProcessor(archive=False)
+        processor.register_importer(OrcaImporter())
+
+        # check states empty
+        with self.store.session_scope():
+            # there must be no states at the beginning
+            states = self.store.session.query(self.store.db_classes.State).all()
+            assert len(states) == 0
+
+            # there must be no platforms at the beginning
+            platforms = self.store.session.query(self.store.db_classes.Platform).all()
+            assert len(platforms) == 0
+
+            # there must be no datafiles at the beginning
+            datafiles = self.store.session.query(self.store.db_classes.Datafile).all()
+            assert len(datafiles) == 0
+
+        # parse the data
+        processor.process(DATA_PATH, self.store, False)
+
+        # check data got created
+        with self.store.session_scope():
+            # there must be states after the import
+            states = self.store.session.query(self.store.db_classes.State).all()
+            assert len(states) == 1
+
+            # there must be platforms after the import
+            platforms = self.store.session.query(self.store.db_classes.Platform).all()
+            assert len(platforms) == 1
+
+            # there must be one datafile afterwards
+            datafiles = self.store.session.query(self.store.db_classes.Datafile).all()
+            assert len(datafiles) == 1
+
+            results = self.store.session.query(self.store.db_classes.State).all()
+
+            assert results[0].platform.name.upper() == "NONSUCH"
