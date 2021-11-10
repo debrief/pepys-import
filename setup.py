4ef599082071ab05fb0fa597a27c1533e47973df
Ian Mayo <ian@planetmayo.com>
2021-11-10 10:08:17 +0000

    Bump version: 0.0.31 → 0.0.32

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/debrief/pepys-import",
-    version="0.0.31",
+    version="0.0.32",
     zip_safe=False,
 )
