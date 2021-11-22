70c25bef108f11e6478bc5650d83f48924531e5e
Matt Willis <88482857+mew-nsc@users.noreply.github.com>
2021-11-22 08:15:52 +0000

    Standard NMEA importer no longer attempts to import WECDIS

--- tests/test_load_nmea.py
+++ tests/test_load_nmea.py
@@ -8,6 +8,7 @@ from pepys_import.file.file_processor import FileProcessor
 
 FILE_PATH = os.path.dirname(__file__)
 DATA_PATH = os.path.join(FILE_PATH, "sample_data/track_files/NMEA")
+WECDIS_DATA_PATH = os.path.join(FILE_PATH, "sample_data/wecdis_files/wecdis_sample.log")
 
 
 class TestLoadNMEA(unittest.TestCase):
@@ -61,6 +62,41 @@ class TestLoadNMEA(unittest.TestCase):
             )
             assert len(states) == 1
             assert states[0].time == datetime(2016, 11, 5, 15, 0, 2)
+
+    def test_NMEA_ignores_WECDIS(self):
+        processor = FileProcessor(archive=False)
+        processor.register_importer(NMEAImporter())
+
+        # check states empty
+        with self.store.session_scope():
+            # there must be no states at the beginning
+            states = self.store.session.query(self.store.db_classes.State).all()
+            self.assertEqual(len(states), 0)
+
+            # there must be no platforms at the beginning
+            platforms = self.store.session.query(self.store.db_classes.Platform).all()
+            self.assertEqual(len(platforms), 0)
+
+            # there must be no datafiles at the beginning
+            datafiles = self.store.session.query(self.store.db_classes.Datafile).all()
+            self.assertEqual(len(datafiles), 0)
+
+        # parse the folder
+        processor.process(WECDIS_DATA_PATH, self.store, False)
+
+        # check data got created
+        with self.store.session_scope():
+            # there must be states after the import
+            states = self.store.session.query(self.store.db_classes.State).all()
+            self.assertEqual(len(states), 0)
+
+            # there must be platforms after the import
+            platforms = self.store.session.query(self.store.db_classes.Platform).all()
+            self.assertEqual(len(platforms), 0)
+
+            # there must be one datafile afterwards
+            datafiles = self.store.session.query(self.store.db_classes.Datafile).all()
+            self.assertEqual(len(datafiles), 0)
 
 
 if __name__ == "__main__":
