09adec08f22b0de33261ff44bb7f82210bdc8e60
Matt Willis <88482857+mew-nsc@users.noreply.github.com>
2021-11-24 14:57:13 +0000

    Development notes prior to implementation

--- importers/full_shore_importer.py
+++ importers/full_shore_importer.py
@@ -23,3 +23,23 @@ class FullShoreImporter(Importer):
 
     def can_load_this_file(self, file_contents):
         return True
+
+    # TODO:
+    # - Date time handling
+    # - Platform / sensor (will need to ask for platform)
+    # - Determine which of the file formats we've actually got
+    # Will need to check the data to see if there is a difference in length
+    # Otherwise, will need to get column names
+    # - Read in ownship positions
+    # - Read in tracks/contacts
+
+    # Some complexities:
+    # - We have two different formatted files (Sept 20 / Jan 21 formatted one way, Mar 21 formatted differently)
+    # - Full Shore format has a range of different sources of data, with any
+    # being present on a row (but only one present per row) so ownship tracks
+    # are mixed up with contacts
+    #    - To minimise possible issues with classification of importer,
+    #    check if ownship and then parse everything else as contacts
+    # - There are a lot of columns in these data files, with lots of the important
+    # ones being towards the end of the table. This may cause issues with the
+    # hightlighting.
---
+++ tests/test_load_full_shore.py
@@ -0,0 +1,6 @@
+# Important tests:
+# - Test both formats
+# - Date/time parsing
+# - Lat/long parsing
+# - Ownship vs contact
+# - Certain vs uncertain positional data
