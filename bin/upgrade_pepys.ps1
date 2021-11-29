3d7bf276a0e396f8850ba8e7eb43fcfacbd50535
Robin Wilson <robin@rtwilson.com>
2021-11-29 17:23:45 +0000

    Copy to correct place, with Verbose and Force flags, ignoring .git directory

--- bin/upgrade_pepys.ps1
+++ bin/upgrade_pepys.ps1
@@ -1,9 +1,14 @@
 $network_path = python -c "import config; print(config.NETWORK_MASTER_INSTALL_PATH)"
 
+if ($network_path -eq "") {
+    Write-Output "Network path not set in config file, cannot copy."
+    Exit 1
+}
+
 Write-Output "Copying new version of Pepys from $network_path"
 
 try {
-    Copy-Item -Path $network_path -Destination "..\" -Recurse
+    Copy-Item -Path "$network_path\*" -Destination "..\" -Recurse -Force -Verbose -Exclude ".git"
 }
 catch {
     Write-Output $_
